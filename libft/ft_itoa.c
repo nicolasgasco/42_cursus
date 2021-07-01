162abfa7f2b3cd35151dba79883fc31a5aa5ba9b
Nicolas Gasco <nicolas.gasco92@gmail.com>
2021-07-01 12:26:11 +0200

    Getting one error I can't solve

--- libft/ft_itoa.c
+++ libft/ft_itoa.c
@@ -2,28 +2,56 @@
 #include "libft.h"
 #include <bsd/string.h>
 
+static int	ft_cal_number_digits(int n)
+{
+	int	res;
+
+	res = 0;
+	if (n == -2147483648)
+		return (11);
+	if (n < 0)
+	{
+		res++;
+		n *= -1;
+	}
+	else if (n == 0)
+		return 1;
+	while (n > 0)
+	{
+		res++;
+		n /= 10;
+	}
+	return res;
+}
 
 char	*ft_itoa(int n)
 {
-	int		mult;
 	char	*res;
-	int		counter;
-	int		dummy;
+	int		num_digits;
+	int		j;
 
-	dummy = n;
-	mult = 1;
-	while (dummy > 1)
+	num_digits = ft_cal_number_digits(n);
+	res = malloc((num_digits + 1) * sizeof(char));
+	if (n == -2147483648)
 	{
-		dummy /= 10;
-		mult *= 10;
+		res = "-2147483648";
+		return (res) ;
 	}
-	res = malloc((counter + 1) * sizeof(char));
-	res = "";
-	while (n > 0)
+	if (res == NULL)
+		return (NULL);
+	j = num_digits - 1;
+	if (n < 0)
 	{
-		printf("N is %d\n", n%10);
-		ft_strlcat("1", res, 1);
-		n /= 10;
+		res[0] = '-';
+		n *= -1;
 	}
-	return "ciao";
+	do
+	{
+		res[j] = (n % 10) + '0';
+		n = n / 10;
+		j--;
+	}
+	while (j >= 0 && n > 0);
+	res[num_digits] = '\0';
+	return res;
 }
