3ee51ddf818b1b81ed4b015ac4441ace415ffb2d
Nicolas Gasco <nicolas.gasco92@gmail.com>
2021-07-29 16:26:18 +0200

    Refined script

--- born2beroot/monitoring.sh
+++ born2beroot/monitoring.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-#!/bin/bash
-
 arch=$(uname -a)
 cpu_p=$(lscpu | grep Socket | awk '{print $2}')
 cpu_v=$(cat /proc/cpuinfo | grep processor | wc -l)
 mem_us=$(free -m | awk NR==2 | awk '{print $3}')
 mem_tot=$(free -m | awk NR==2 | awk '{print $2}')
 mem_usage=$(free -t | tail -n 1 | awk '{printf "%.1f", $3*100/$2}')
+disk_used=$(df -m | awk '{if (NR>1) print $3}' | awk '{n += $1}; END{print n}')
+disk_tot=$(df -m | awk '{if (NR>1) print $4}' | awk '{n += $1}; END{printf "%.1f", n/1000}')
+disk_tot_m=$(df -m | awk '{if (NR>1) print $4}' | awk '{n += $1}; END{print n}')
+let "disk_usage = disk_used * 100 / disk_tot_m"
 cpu_load=$(top -bn1 | grep '^%Cpu' | cut -c 9- | xargs | awk '{printf "%.1f", $1 + $3}')
 last_boot=$(who -b | grep -oP '\d.+')
 ip=$(hostname -I)
@@ -19,8 +21,8 @@ users=$(who | wc -l)
 wall "  #Architecture: $arch
         #CPU physical: $cpu_p
         #vCPU: $cpu_v
-        #Memory Usage: $mem_us/$mem_tot ($mem_usage%)
-        #Disk Usage:
+        #Memory Usage: $mem_us/$mem_tot MB ($mem_usage%)
+        #Disk Usage: $disk_used/$disk_tot Gb ($disk_usage%)
         #CPU load: $cpu_load%
         #Last boot: $last_boot
         #LVM use: $lvm
@@ -29,6 +31,3 @@ wall "  #Architecture: $arch
         #Network: IP $ip ($mac)
         #Sudo: $sudo cmd
         "
-
-
-
