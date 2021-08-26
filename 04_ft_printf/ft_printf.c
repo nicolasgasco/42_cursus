48f495c2942f27ae4a7aa95a9681e5bb2168f140
Nicolas Gasco <nicolas.gasco92@gmail.com>
2021-08-26 12:33:52 +0200

    Started printf

---
+++ 04_ft_printf/ft_printf.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ngasco <ngasco@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/26 10:57:57 by ngasco            #+#    #+#             */
+/*   Updated: 2021/08/26 12:20:23 by ngasco           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+// c = single character
+// s = string
+// void * printed in hexadecimal
+// d = decimal number
+// i = integer
+//  u = unsigned decimal
+// x = hexadec with lower
+// X = hexadec with upper
+// %% = percent
+
+int	ft_printf(const char *str, ...)
+{
+    va_list arg_list;
+
+    return (0);
+}
