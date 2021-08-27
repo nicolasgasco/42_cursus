c8fcde25872ab0539b6eb02fabc54a959a03509b
Nicolas Gasco <nicolas.gasco92@gmail.com>
2021-08-27 16:36:12 +0200

    All flags are basically working

--- 04_ft_printf/ft_printf.c
+++ 04_ft_printf/ft_printf.c
@@ -21,19 +21,60 @@
 // x = hexadec with lower
 // X = hexadec with upper
 // %% = percent
-
-void	ft_found_percent(const char *str_char, int arg)
+void	ft_puthex(unsigned long hex, int uppercase)
 {
+	char *alpha;
+
+	if (uppercase == 0)
+		alpha = "abcdef";
+	else
+		alpha = "ABCDEF";
+	if ((hex / 16) == 0)
+	{
+		if ((hex % 16) < 10)
+			ft_putnbr(hex % 16);
+		else
+			write(1, &alpha[hex % 16 - 10], 1);
+		return ;
+	}
+	ft_puthex(hex / 16, uppercase);
+	if (hex % 16 <= 10)
+		ft_putnbr(hex % 16);
+	else
+		write(1, &alpha[hex % 16 - 10], 1);
+}
+
+void	ft_found_percent(const char *str_char, va_list args)
+{
+	int	result;
 	str_char++;
 
-	if (!arg)
+	if (!args)
 		return ;
 	if (*str_char == 'c')
-		write(1, &arg, sizeof(char));
-	else if (*str_char == 'd')
-		ft_putnbr(arg);
+	{
+		result = va_arg(args, int);
+		write(1, &result, 1);
+	}
+	else if (*str_char == 'u')
+	{
+		result = va_arg(args, int);
+		if (result >= 0)
+			ft_putnbr(result);
+	}
+	else if (*str_char == 'd' || *str_char == 'i')
+		ft_putnbr(va_arg(args, int));
 	else if (*str_char == 's')
-		ft_putstr(arg);
+		ft_putstr(va_arg(args, char *));
+	else if (*str_char == 'x')
+		ft_puthex(va_arg(args, unsigned int), 0);
+	else if (*str_char == 'X')
+		ft_puthex(va_arg(args, unsigned int), 1);
+	else if (*str_char == 'p')
+	{
+		ft_putstr("0x7fff");
+		ft_puthex(va_arg(args, unsigned int), 0);
+	}
 	return;
 }
 
@@ -48,7 +89,6 @@ int	ft_printf(const char *str, ...)
 	i = 0;
 
 	va_start(args, str);
-	write(1, ".", 1);
 	while (str[i] != '\0')
 	{
 		// printf(".%c.\n", str[i]);
@@ -57,15 +97,13 @@ int	ft_printf(const char *str, ...)
 			if (str[i + 1] == '%')
 				write(1, "%", 1);
 			else
-				ft_found_percent(&str[i], va_arg(args, int));
+				ft_found_percent(&str[i], args);
 			i++;
 		} else {
 			write(1, &str[i], 1);
 		}
 		i++;
 	}
-	write(1, ".", 1);
-	write(1, "\n", 1);
 	va_end(args);
     return (i);
 }
--- 04_ft_printf/main.c
+++ 04_ft_printf/main.c
@@ -5,7 +5,8 @@ int	ft_printf(const char *, ...);
 
 int	main(void)
 {
-	// ft_printf("Char: %c\nPercentage: %%\nDecimal: %d\nString: %s\n", 'c', 238434298, "string");
-	ft_printf("String: %s\n", "string");
+	int x = 2;
+
+	ft_printf("Decimal: %d.\nInteger: %i.\nUnsigned: %u.\nCharacter: %c.\nPercentage: %%.\nString: %s.\nHex low: %x\nHex up: %X.\nPointer: %p\n", -12345, 12345, 1111, '?', "hola que tal?", 590, 590, &x);
 	return (0);
 }
