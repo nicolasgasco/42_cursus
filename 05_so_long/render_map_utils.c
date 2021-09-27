5e8c951522767d0e624fa3a5a22280026e2d2a8e
Nicolas Gasco <nicolas.gasco92@gmail.com>
2021-09-27 20:32:59 +0200

    Added simple function to compare strings

--- 05_so_long/render_map_utils.c
+++ 05_so_long/render_map_utils.c
@@ -62,3 +62,17 @@ char	*ft_itoa(int n)
 	}
 	return (res);
 }
+
+int	ft_compare_strings(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+			return (0);
+		i++;
+	}
+	return (1);
+}
