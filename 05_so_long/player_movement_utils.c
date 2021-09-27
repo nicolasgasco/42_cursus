5d21842c257ca3c7d1afac406c3a26d34280df3a
Nicolas Gasco <nicolas.gasco92@gmail.com>
2021-09-27 20:33:10 +0200

    Moved functions to separate file

---
+++ 05_so_long/player_movement_utils.c
@@ -0,0 +1,39 @@
+#include "so_long.h"
+
+void    ft_swap_tiles(char *tile1, char *tile2)
+{
+	char	temp;
+
+	// printf("Swapping tiles...\n");
+	temp = *tile1;
+	*tile1 = *tile2;
+	*tile2 = temp;
+}
+
+void    ft_remove_collectible(char *player, char *collect)
+{
+	char	temp;
+
+	// printf("Taking collectible...\n");
+	*collect = *player;
+	*player = '0';
+}
+
+void    ft_found_exit(char *player, char *collect)
+{
+	char	temp;
+
+	// printf("Found exit...\n");
+	*collect = *player;
+	*player = '0';
+}
+
+void    ft_end_game(t_map *map)
+{
+    if (map->end_game == 1)
+	{
+		ft_put_str("Well done, pirate!\nSo long...\n");
+		mlx_destroy_window(map->mlx, map->win);
+		exit(0);
+	}
+}
