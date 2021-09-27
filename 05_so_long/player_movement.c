b26316053e1f06de4dd8f9eafbd998f2a762f876
Nicolas Gasco <nicolas.gasco92@gmail.com>
2021-09-27 20:44:42 +0200

    Refactored file

--- 05_so_long/player_movement.c
+++ 05_so_long/player_movement.c
@@ -1,31 +1,35 @@
 #include "so_long.h"
 
-void    ft_swap_tiles(char *tile1, char *tile2)
+void	ft_putchar_fd(char c, int fd)
 {
-	char	temp;
-
-	// printf("Swapping tiles...\n");
-	temp = *tile1;
-	*tile1 = *tile2;
-	*tile2 = temp;
+	write(fd, &c, 1);
 }
 
-void    ft_remove_collectible(char *player, char *collect)
+void	ft_putnbr_fd(int n, int fd)
 {
-	char	temp;
-
-	// printf("Taking collectible...\n");
-	*collect = *player;
-	*player = '0';
+	if (n == -2147483648)
+	{
+		ft_putnbr_fd(n / 10, fd);
+		ft_putchar_fd('8', fd);
+	}
+	else if (n < 0)
+	{
+		ft_putchar_fd('-', fd);
+		ft_putnbr_fd(-n, fd);
+	}
+	else
+	{
+		if (n > 9)
+			ft_putnbr_fd(n / 10, fd);
+		ft_putchar_fd((n % 10) + '0', fd);
+	}
 }
 
-void    ft_found_exit(char *player, char *collect)
-{
-	char	temp;
-
-	// printf("Found exit...\n");
-	*collect = *player;
-	*player = '0';
+void	ft_put_moves(t_map *map)
+{		
+		ft_put_str("Moves: ");
+		ft_putnbr_fd(map->moves, 1);
+		ft_put_str("\n");
 }
 
 void    ft_move_player(t_map *map, int x, int y)
@@ -37,19 +41,14 @@ void    ft_move_player(t_map *map, int x, int y)
     {
 		map->moves += 1;
 		if (map->map[map->p_y + y][map->p_x + x] == '0')
-		{
-			// printf("Moving freely\n");
 			ft_swap_tiles(&map->map[map->p_y][map->p_x], &map->map[map->p_y + y][map->p_x + x]);
-		}
 		else if (map->map[map->p_y + y][map->p_x + x] == 'C')
 		{
-			// printf("Found a collectible\n");
 			ft_remove_collectible(&map->map[map->p_y][map->p_x], &map->map[map->p_y + y][map->p_x + x]);
 			map->n_collect--;
 		}
 		else if (map->map[map->p_y + y][map->p_x + x] == 'E')
 		{
-			// printf("Found an exit\n");
 			if (map->n_collect == 0)
 			{
 				ft_found_exit(&map->map[map->p_y][map->p_x], &map->map[map->p_y + y][map->p_x + x]);
@@ -58,16 +57,8 @@ void    ft_move_player(t_map *map, int x, int y)
 			else
 				ft_put_str("Collect all objects first.\n");
 		}
-		ft_put_str("Moves: ");
-		ft_put_str(ft_itoa(map->moves));
-		ft_put_str("\n");
+		ft_put_moves(map);
+		ft_populate_map(map, 40, 0);
     }
-	ft_populate_map(map, 40, 0);
-	if (map->end_game == 1)
-	{
-		ft_put_str("Well done, pirate!\nSo long...\n");
-		mlx_destroy_window(map->mlx, map->win);
-		exit(0);
-	}
-
+	ft_end_game(map);
 }
