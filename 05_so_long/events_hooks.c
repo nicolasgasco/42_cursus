7fe43908e07f902eef36bc70a29d7b7d9a357001
Nicolas Gasco <nicolas.gasco92@gmail.com>
2021-09-27 20:32:11 +0200

    Removed printf

--- 05_so_long/events_hooks.c
+++ 05_so_long/events_hooks.c
@@ -10,30 +10,15 @@ int ft_close_window(t_map *map)
 int    ft_keyboard_events(int keycode, t_map *map)
 {
     if (keycode == 97 || keycode == 65361)
-    {
-        // printf("You pressed Left\n");
         ft_move_player(map, -1, 0);
-    }
     else if (keycode == 119 || keycode == 65362)
-    {
-        // printf("You pressed Up\n");
         ft_move_player(map, 0, -1);
-    }
     else if (keycode == 100 || keycode == 65363)
-    {
-        // printf("You pressed Right\n");
         ft_move_player(map, 1, 0);
-    }
     else if (keycode == 115 || keycode == 65364)
-    {
-        // printf("You pressed Down\n");
         ft_move_player(map, 0, 1);
-    }
     else if (keycode == 65307)
-    {
-        // printf("You pressed Esc\n");
         ft_close_window(map);
-    }
     return (1);
 }
 
