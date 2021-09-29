f91220a1957826a705b33125a2a7a959a2c68cef
Nicolas Gasco <nicolas.gasco92@gmail.com>
2021-09-29 20:44:28 +0200

    Removed uncrowded file

--- 05_so_long/so_long.c
+++ 05_so_long/so_long.c
@@ -12,6 +12,18 @@
 
 #include "so_long.h"
 
+void	ft_put_str(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+}
+
 int	main(int argc, char **argv)
 {
 	int		file_size;
