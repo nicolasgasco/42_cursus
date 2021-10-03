6fe069bc6aa3dd141518ed9aa6b039f5e06fed39
Nicolas Gasco <nicolas.gasco92@gmail.com>
2021-10-03 20:24:54 +0200

    Fixed issue with extension checking

--- 05_so_long/bonus/so_long_bonus.c
+++ 05_so_long/bonus/so_long_bonus.c
@@ -23,6 +23,7 @@ void	ft_put_str(char *str)
 		i++;
 	}
 }
+
 int	ft_validate_filename(char *file)
 {
 	int	i;
@@ -31,20 +32,21 @@ int	ft_validate_filename(char *file)
 	while (file[i] != '.' && file[i] != '\0')
 		i++;
 	if (file[i] == '\0')
-		return (0);
+		return (-1);
 	if (file[i + 1] == 'b' && file[i + 2] == 'e' && file[i + 3] == 'r' && file[i + 4] == '\0')
-		return (1);
-	return (0);
+		return (0);
+	return (-1);
 }
 
 int	ft_check_file_extension(char *file)
 {
-	if (ft_validate_filename(file) != 1)
+	if (ft_validate_filename(file) == -1)
 	{
 		ft_put_str("Error\n");
 		ft_put_str("Provide a .ber file.\n");
 		return (-1);
 	}
+	return (0);
 }
 
 int	main(int argc, char **argv)
@@ -67,7 +69,8 @@ int	main(int argc, char **argv)
 	}
 	if (argc == 2)
 	{
-		ft_check_file_extension(argv[1]);
+		if (ft_check_file_extension(argv[1]) == -1)
+			return (-1);
 		ft_open_for_validation(argv[1], &file_size);
 		map.map = ft_create_bi_array(argv[1], file_size, &map);
 		ft_render_map(&map);
