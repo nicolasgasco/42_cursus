8e3d54f60f006733994fe6a2cc9c7d2d46475499
Nicolas Gasco <nicolas.gasco92@gmail.com>
2021-10-03 19:53:07 +0200

    Added file extension validation

--- 05_so_long/bonus/so_long_bonus.c
+++ 05_so_long/bonus/so_long_bonus.c
@@ -23,6 +23,29 @@ void	ft_put_str(char *str)
 		i++;
 	}
 }
+int	ft_validate_filename(char *file)
+{
+	int	i;
+
+	i = 0;
+	while (file[i] != '.' && file[i] != '\0')
+		i++;
+	if (file[i] == '\0')
+		return (0);
+	if (file[i + 1] == 'b' && file[i + 2] == 'e' && file[i + 3] == 'r' && file[i + 4] == '\0')
+		return (1);
+	return (0);
+}
+
+int	ft_check_file_extension(char *file)
+{
+	if (ft_validate_filename(file) != 1)
+	{
+		ft_put_str("Error\n");
+		ft_put_str("Provide a .ber file.\n");
+		return (-1);
+	}
+}
 
 int	main(int argc, char **argv)
 {
@@ -44,6 +67,7 @@ int	main(int argc, char **argv)
 	}
 	if (argc == 2)
 	{
+		ft_check_file_extension(argv[1]);
 		ft_open_for_validation(argv[1], &file_size);
 		map.map = ft_create_bi_array(argv[1], file_size, &map);
 		ft_render_map(&map);
