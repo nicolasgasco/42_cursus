94435f5bf5769c1bab216809332dc21a97bb0705
Nicolas Gasco <nicolas.gasco92@gmail.com>
2021-10-10 15:40:37 +0200

    Added correct return value

--- 06_exam_02/ft_printf/ft_printf.c
+++ 06_exam_02/ft_printf/ft_printf.c
@@ -4,99 +4,98 @@
 
 #include <stdio.h>
 
-void	ft_put_str(const char *s)
+void	ft_put_str(const char *s, int *counter)
 {
 	int	i;
 
 	i = 0;
 	while (s[i] != '\0')
 	{
-		write(1, &s[i], 1);
+		*counter += write(1, &s[i], 1);
 		i++;
 	}
 }
 
-void	ft_put_int(int	n)
+void	ft_put_int(int	n, int *counter)
 {
 	char 					result;
 	
 	if (n < 0)
-		write(1, "-", 1);
+		*counter += write(1, "-", 1);
 	if ((unsigned int)n < 9)
 	{
 		result = (unsigned int)n + '0';
-		write(1, &result, 1);
+		*counter += write(1, &result, 1);
 	}
 	else
 	{
 		result = ((unsigned int)n % 10) + '0';
-		ft_put_int((unsigned int)n / 10);
-		write(1, &result, 1);
+		ft_put_int((unsigned int)n / 10, counter);
+		*counter += write(1, &result, 1);
 	}
 }
 
-void	ft_put_hex(unsigned int n)
+void	ft_put_hex(unsigned int n, int *counter)
 {
 	char	result;
 
 	if (n <= 9)
 	{
 		result = n + '0';
-		write(1, &result, 1);
+		*counter += write(1, &result, 1);
 	}
 	else
 	{
 		result = n % 16;
-		ft_put_hex(n / 16);
+		ft_put_hex(n / 16, counter);
 		if (result <= 9)
 		{
 			result = result + '0';
-			write(1, &result, 1);
+			*counter += write(1, &result, 1);
 		}
 		else
 		{
 			result = ((n % 16 - 9) + '0') + 48;
-			write(1, &result, 1);
+			*counter += write(1, &result, 1);
 		}
 	}	
 }
 
-void	ft_flags(char c, va_list args)
+void	ft_flags(char c, va_list args, int *counter)
 {
 	if (c == 's')
-		ft_put_str(va_arg(args, const char *));
+		ft_put_str(va_arg(args, const char *), counter);
 	else if (c == 'd')
-	{
-		ft_put_int(va_arg(args, int));
-	}
+		ft_put_int(va_arg(args, int), counter);
 	else if (c == 'x')
-	{
-		ft_put_hex(va_arg(args, unsigned int));
-	}
+		ft_put_hex(va_arg(args, unsigned int), counter);
 }
 
 int	ft_printf(const char *s, ...)
 {
-	int			i;
+	int	i;
 	va_list	args;
+	int	counter;
 
 	i = 0;
+	counter = 0;
 	va_start(args, s);
 	while (s[i] != '\0')
 	{
 		if (s[i] == '%')
 		{
 			if (s[i + 1] == '%')
-				write(1, "%", 1);
+				counter += write(1, "%", 1);
 			else
-				ft_flags(s[i + 1], args);
+				ft_flags(s[i + 1], args, &counter);
 			i++;
 		}
 		else
-			write(1, &s[i], 1);
+			counter += write(1, &s[i], 1);
 		i++;
 	}
 	va_end(args);
+	return (counter);
 	return (0);
 }
 
@@ -116,4 +115,14 @@ int	main(void)
 	printf("\n");
 	printf("Int max is %d\n", INT_MAX);
 	ft_printf("Int max is %d\n", INT_MAX);
+	printf("\n");
+	printf("Return tests:\n");	
+	printf("Printf: %d\n", printf("Test%d (%%d)\n", 1));
+	printf("ft_printf: %d\n", ft_printf("Test%d (%%d)\n", 1));
+	printf("\n");	
+	printf("Printf: %d\n", printf("%s2 (%%s)\n", "Test"));
+	printf("Ft_printf: %d\n", ft_printf("%s2 (%%s)\n", "Test"));
+	printf("\n");
+	printf("Printf: %d\n", printf("Test %x (%%x)\n", 3));
+	printf("Ft_printf: %d\n", ft_printf("Test %x (%%x)\n", 3));
 }
