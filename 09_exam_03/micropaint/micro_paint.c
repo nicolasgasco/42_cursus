fe0982b3bc2f0f8820d479b33ab680e77fb450b3
Nicolas Gasco <nicolas.gasco92@gmail.com>
2021-11-28 12:00:32 +0100

    Tests WIP

--- 09_exam_03/micropaint/micro_paint.c
+++ 09_exam_03/micropaint/micro_paint.c
@@ -3,27 +3,33 @@
 #include <unistd.h>
 #include <stddef.h>
 
-struct board
-{
-	int		width;
-	int		height;
-	char	empty;
-	char	**tab;
-	int		error;
-};
+char **tab;
 
-void ft_print_tab(struct board *board)
+// Board line
+int board_width = 0;
+int board_height = 0;
+char empty = '0';
+
+// Shape line
+char shape;
+float x_coord;
+float y_coord;
+float width;
+float height;
+char stroke;
+
+void ft_print_tab()
 {
 	int x;
 	int y;
 
 	y = 0;
-	while (board->tab[y] != NULL)
+	while (tab[y] != NULL)
 	{
 		x = 0;
-		while (board->tab[y][x] != '\0')
+		while (tab[y][x] != '\0')
 		{
-			printf("%c", board->tab[y][x]);
+			printf("%c", tab[y][x]);
 			x++;
 		}
 		printf("\n");
@@ -31,100 +37,78 @@ void ft_print_tab(struct board *board)
 	}
 }
 
-void ft_print_struct(struct board board)
+void ft_print_struct()
 {
 	printf("First line:\n");
-	printf("Board.width is %d\n", board.width);
-	printf("Board.height is %d\n", board.height);
-	printf("Board.empty is %c\n", board.empty);
-	// ft_print_tab(&board);
+	printf("width is %d\n", width);
+	printf("height is %d\n", height);
+	printf("empty is %c\n", empty);
+	// ft_print_tab(&;
 }
 
-void ft_draw_board(struct board *board)
+void ft_draw_board()
 {
 	int x;
 	int y;
 
-	board->tab = malloc(sizeof(char **) * (board->height + 1));
+	tab = malloc((sizeof(char *) * board_height) + 1);
 	y = 0;
-	while (y < board->height)
+	while (y < board_height)
 	{
 		x = 0;
-		board->tab[y] = malloc((sizeof(char)) * board->height + 1);
-		while (x < board->width)
+		tab[y] = malloc((sizeof(char)) * board_width + 1);
+		while (x < board_width)
 		{
-			board->tab[y][x] = board->empty;
+			tab[y][x] = empty;
 			x++;
 		}
-		board->tab[y][x] = '\0';
+		tab[y][x] = '\0';
 		y++;
 	}
-	board->tab[y] = NULL;
+	tab[y] = NULL;
 }
 
-void ft_scan_line(struct board *board, FILE *file)
+void ft_draw_shape()
 {
-	int width;
-	int height;
-	char empty;
+	And a rectangle with a top left corner (Xtl, Ytl) and a bottom right corner (Xbr, Ybr)
+If Xtl <= Xa <= Xbr and Ytl <= Ya <= Ybr then the point is in the rectangle
 
-	fscanf(file, "%d %d %c\n", &width, &height, &empty);
-	board->width = width;
-	board->height = height;
-	board->empty = empty;
-}
-
-void ft_draw_shape(struct board *board, int y_coord, int x_coord, int width, int height, char stroke, char shape)
-{
 	int y = 0;
-	while (board->tab[y] != NULL)
+
+	while (tab[y] != NULL)
 	{
 		int x = 0;
-		while (board->tab[y][x] != '\0')
+		while (tab[y][x] != '\0')
 		{
-			if (y == y_coord && (x >= x_coord && x <= (x_coord + width)))
-				board->tab[y][x] = stroke;
-			else if (y == (y_coord + height) && (x >= x_coord && x <= (x_coord + width)))
-				board->tab[y][x] = stroke;
-			else if ((y >= y_coord && y <= y_coord + height) && x == x_coord)
-				board->tab[y][x] = stroke;
-			else if ((y >= y_coord && y <= y_coord + height) && (x == (x_coord + width)))
-				board->tab[y][x] = stroke;
-			if (shape == 'R')
-			{
-				if ((y >= y_coord && y <= (y_coord + height)) && (x >= x_coord && x <= x_coord + width))
-					board->tab[y][x] = stroke;
-			}
+			if (
 			x++;
 		}
 		y++;
 	}
 }
 
-int ft_scan_shape(struct board *board, FILE *file)
+int ft_round_float(float n)
 {
-	char shape;
-	float x_coord;
-	float y_coord;
-	float width;
-	float height;
-	char stroke;
+	n *= 10.00;
+	if ((int)n % 10 > 5)
+		n++;
+	return (int)n / 10;
+}
+
+void ft_scan_line(FILE *file)
+{
+	fscanf(file, "%d %d %c\n", &board_width, &board_height, &empty);
+}
+
+int ft_scan_shape(FILE *file)
+{
 	int result;
 
-	result = fscanf(file, "%c %f %f %f %f %c\n", &shape, &x_coord, &y_coord, &width, &height, &stroke);
-	if (result == -1)
-		return result;
-	if (result < 6)
-	{
-		board->error = 1;
-		return result;
-	}
-	ft_draw_shape(board, (int)y_coord + 1, (int)x_coord + 1, (int)width - 1, (int)height - 1, stroke, shape);
-	return result;
+	return fscanf(file, "%c %f %f %f %f %c\n", &shape, &x_coord, &y_coord, &width, &height, &stroke);
 }
+
 int main(int argc, char *argv[])
 {
-	struct board board;
 	FILE *file;
 	if (argc != 2)
 	{
@@ -132,33 +116,29 @@ int main(int argc, char *argv[])
 		return (1);
 	}
 	file = fopen(argv[1], "r");
-	if (file == NULL)
+	if (!file)
 	{
 		write(1, "Error: Operation file corrupted\n", 32);
 		return (1);
 	}
-	ft_scan_line(&board, file);
-	if (board.width > 300 || board.height > 300)
+	ft_scan_line(file);
+	if (width > 300 || height > 300)
 	{
 		write(1, "Error: Operation file corrupted\n", 32);
 		return (1);
 	}
-	if (board.empty == 'r' || board.empty == 'R')
+	ft_draw_board();
+	while (ft_scan_shape(file) == 6)
 	{
-		write(1, "Error: Operation file corrupted\n", 32);
-		return (1);
-	}
-	ft_draw_board(&board);
-	while (ft_scan_shape(&board, file) != -1)
-	{
-		if (board.error == 1)
+		if (width <= 0 || height <= 0 || (shape != 'R' && shape != 'r'))
 		{
 			write(1, "Error: Operation file corrupted\n", 32);
-			return (1);	
+			return (1);
 		}
+		ft_draw_shape();
 	}
-	ft_print_tab(&board);
-	// ft_print_struct(board);
+	ft_print_tab();
+	// ft_print_struct();
 	fclose(file);
 	return (0);
 }
