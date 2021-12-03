b1e28341eb72d432b5a13b7115e7609805cc0c29
Nicolas Gasco <nicolas.gasco92@gmail.com>
2021-12-03 19:48:28 +0100

    First steps

---
+++ 10_philosophers/philo.c
@@ -0,0 +1,15 @@
+#include "philo.h"
+
+int main(int argc, char *argv[])
+{
+    if (argc < 4 || argc > 6)
+    {
+        ft_putstr("Error: arguments\n");
+        return (1);
+    }
+    if (argv[0])
+    {
+        // Continue
+    }
+    return (0);
+}
---
+++ 10_philosophers/philo.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ngasco <ngasco@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/16 19:01:54 by ngasco            #+#    #+#             */
+/*   Updated: 2021/11/16 19:39:11 by ngasco           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+# include <unistd.h>
+# include <stdio.h>
+
+
+// Put utils
+void    ft_putstr(char *s);
+
+#endif
