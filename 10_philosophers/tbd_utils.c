1e3033e50cc7540ab490b20e63c62374359fd2fa
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 <nicolas.gasco92@gmail.com>
2021-12-05 16:58:50 +0100

    Not showing start time any more

--- 10_philosophers/tbd_utils.c
+++ 10_philosophers/tbd_utils.c
@@ -1,13 +1,13 @@
 #include "philo.h"
 
-void    ft_print_struct(t_data *philo)
+void    ft_print_struct(t_data *common_data)
 {
     printf("\n");
-    printf("Number of philosophers: %d\n", philo->n_philos);
-    printf("Time to death: %d\n", philo->t_death);
-    printf("Time to eat: %d\n", philo->t_eat);
-    printf("Time to sleep: %d\n", philo->t_sleep);
-    printf("Number of eats: %d\n", philo->n_eats);
-    printf("seconds : %ld\nmicro seconds : %d\n\n", philo->t_start.tv_sec, philo->t_start.tv_usec);
+    printf("Number of philosophers: %d\n", common_data->n_philos);
+    printf("Time to death: %d\n", common_data->t_death);
+    printf("Time to eat: %d\n", common_data->t_eat);
+    printf("Time to sleep: %d\n", common_data->t_sleep);
+    printf("Number of eats: %d\n", common_data->n_eats);
+    // printf("seconds : %ld\nmicro seconds : %d\n\n", common_data->t_start.tv_sec, common_data->t_start.tv_usec);
     printf("\n");
 }
