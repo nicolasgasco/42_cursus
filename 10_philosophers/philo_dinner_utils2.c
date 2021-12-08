190a3c93a84267619cab53743f001f41f9a5ef64
Nicolas Gasco <nicolas.gasco92@gmail.com>
2021-12-08 22:55:55 +0100

    Improved forks logic

--- 10_philosophers/philo_dinner_utils2.c
+++ 10_philosophers/philo_dinner_utils2.c
@@ -15,18 +15,18 @@
 void	ft_eat(t_philo *philo, int i, int *fork_l, int *fork_r)
 {
     pthread_mutex_lock(&philo->c_data->forks_mutex[i]);
-    // ft_put_status(philo, 'f');
+    *fork_r = 0;
+    ft_put_status(philo, 'f');
     pthread_mutex_lock(&philo->c_data->forks_mutex[ft_get_i(philo, i - 1)]);
     ft_put_status(philo, 'f');
     gettimeofday(&philo->t_meal, NULL);
-    *fork_r = 0;
     *fork_l = 0;
     ft_put_status(philo, 'e');
     philo->meals++;
     ft_msleep(philo, philo->c_data->t_eat);
-    *fork_r = 1;
     *fork_l = 1;
     pthread_mutex_unlock(&philo->c_data->forks_mutex[i]);
+    *fork_r = 1;
     pthread_mutex_unlock(&philo->c_data->forks_mutex[ft_get_i(philo, i - 1)]);
 }
 
