746a861540731a44d836c8570c18e977c828a884
Nicolas Gasco <73175085+nicolasgasco@users.noreply.github.com>
2021-12-09 12:52:27 +0100

    Refactored code

--- 10_philosophers/philo_dinner_utils2.c
+++ 10_philosophers/philo_dinner_utils2.c
@@ -20,8 +20,8 @@ void	ft_eat(t_philo *philo, int i, int *fork_l, int *fork_r)
     pthread_mutex_lock(&philo->c_data->forks_mutex[ft_get_i(philo, i - 1)]);
     *fork_l = 0;
     ft_put_status(philo, 'f')
-    gettimeofday(&philo->t_meal, NULL);
     philo->meals++;
+    gettimeofday(&philo->t_meal, NULL);
     ft_put_status(philo, 'e');
     ft_msleep(philo, philo->c_data->t_eat);
     *fork_l = 1;
