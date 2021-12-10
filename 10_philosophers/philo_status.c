76739a01dd3ce7e12429b6e222ca0742b5f43e48
Nicolas Gasco <nicolas.gasco92@gmail.com>
2021-12-10 16:19:39 +0100

    Added status protection

--- 10_philosophers/philo_status.c
+++ 10_philosophers/philo_status.c
@@ -14,11 +14,14 @@
 
 void	ft_put_status(t_philo *philo, char flag)
 {
-	pthread_mutex_lock(&philo->c_data->status_mutex);
-	ft_put_timestamp(philo);
-	ft_put_id(philo->i_philo + 1);
-	ft_put_action(philo, flag);
-	pthread_mutex_unlock(&philo->c_data->status_mutex);
+	if (philo->c_data->end == 0)
+	{
+		pthread_mutex_lock(&philo->c_data->status_mutex);
+		ft_put_timestamp(philo);
+		ft_put_id(philo->i_philo + 1);
+		ft_put_action(philo, flag);
+		pthread_mutex_unlock(&philo->c_data->status_mutex);
+	}
 }
 
 void	ft_put_timestamp(t_philo *philo)
