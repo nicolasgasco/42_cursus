d558425877acdcddafe1c7bc757bab36120c1d2f
Nicolas Gasco <nicolas.gasco92@gmail.com>
2021-12-11 01:48:29 +0100

    Improved eating logic

--- 10_philosophers/philo_actions.c
+++ 10_philosophers/philo_actions.c
@@ -17,9 +17,9 @@ void	ft_eat(t_philo *philo, int i, int *fork_l, int *fork_r)
 	if (philo->c_data->end == 0)
 	{
 		pthread_mutex_lock(&philo->c_data->forks_mutex[i]);
+		pthread_mutex_lock(&philo->c_data->forks_mutex[ft_get_i(philo, i - 1)]);
 		*fork_r = 0;
 		ft_put_status(philo, 'f');
-		pthread_mutex_lock(&philo->c_data->forks_mutex[ft_get_i(philo, i - 1)]);
 		*fork_l = 0;
 		ft_put_status(philo, 'f');
 		philo->meals++;
@@ -27,8 +27,8 @@ void	ft_eat(t_philo *philo, int i, int *fork_l, int *fork_r)
 		ft_put_status(philo, 'e');
 		ft_msleep(philo, philo->c_data->t_eat);
 		*fork_l = 1;
-		pthread_mutex_unlock(&philo->c_data->forks_mutex[i]);
 		*fork_r = 1;
+		pthread_mutex_unlock(&philo->c_data->forks_mutex[i]);
 		pthread_mutex_unlock(&philo->c_data->forks_mutex[ft_get_i(philo, i - 1)]);
 	}
 }
