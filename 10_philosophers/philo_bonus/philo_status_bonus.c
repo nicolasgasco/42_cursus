b9984148fdfd46cda0a8f6191850ad2d699631c4
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 <nicolas.gasco92@gmail.com>
2022-01-22 18:32:22 +0100

    Added status semaphore

--- 10_philosophers/philo_bonus/philo_status_bonus.c
+++ 10_philosophers/philo_bonus/philo_status_bonus.c
@@ -12,28 +12,29 @@
 
 #include "philo_bonus.h"
 
-void	ft_put_status(t_philo *philo, char flag)
+void ft_put_status(t_philo *philo, char flag)
 {
-	int	timestamp;
+	int timestamp;
 
 	timestamp = ft_calc_timestamp(philo->c_data->t_start);
-	// if (philo->c_data->end == 0)
-	// {
-		if (flag == 'f')
-			printf("%d %d has taken a fork\n", timestamp, philo->i_philo + 1);
-		else if (flag == 'e')
-			printf("%d %d is eating\n", timestamp, philo->i_philo + 1);
-		else if (flag == 's')
-			printf("%d %d is sleeping\n", timestamp, philo->i_philo + 1);
-		else if (flag == 't')
-			printf("%d %d is thinking\n", timestamp, philo->i_philo + 1);
-	// }
+	sem_wait(philo->c_data->status_sem);
+	if (flag == 'f')
+		printf("%d %d has taken a fork\n", timestamp, philo->i_philo + 1);
+	else if (flag == 'e')
+		printf("%d %d is eating\n", timestamp, philo->i_philo + 1);
+	else if (flag == 's')
+		printf("%d %d is sleeping\n", timestamp, philo->i_philo + 1);
+	else if (flag == 't')
+		printf("%d %d is thinking\n", timestamp, philo->i_philo + 1);
+	sem_post(philo->c_data->status_sem);
 }
 
-void	ft_put_death(t_philo *philo)
+void ft_put_death(t_philo *philo)
 {
-	int	timestamp;
+	int timestamp;
 
+	sem_wait(philo->c_data->status_sem);
 	timestamp = ft_calc_timestamp(philo->c_data->t_start);
 	printf("%d %d died\n", timestamp, philo->i_philo + 1);
+	sem_post(philo->c_data->status_sem);
 }
