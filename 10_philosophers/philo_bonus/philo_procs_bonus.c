9916b30dd7e0da5af813b218fa376a439f32a435
Nicolas Gasco <nicolas.gasco92@gmail.com>
2022-01-23 10:11:00 +0100

    Joining threads when not needed anymore

--- 10_philosophers/philo_bonus/philo_procs_bonus.c
+++ 10_philosophers/philo_bonus/philo_procs_bonus.c
@@ -68,7 +68,6 @@ void	ft_create_procs(t_data *c_data)
 			ft_init_philo(philo, c_data, i);
 			pthread_create(&philo->death_check, NULL, ft_death_routine, philo);
 			ft_routine(philo);
-			pthread_join(philo->death_check, NULL);
 		}
 		i++;
 	}
