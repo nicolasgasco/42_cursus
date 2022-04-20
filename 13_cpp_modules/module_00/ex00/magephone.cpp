9b6a06d8cd5ab8c7362e86b6bb5839b7515bf06e
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 <nicolas.gasco92@gmail.com>
2022-04-20 17:16:13 +0200

    First attempt at exercise 1

---
+++ 13_cpp_modules/module_00/ex00/magephone.cpp
@@ -0,0 +1,16 @@
+#include <cctype>
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+    if (argc == 1)
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+    else {
+        for (int i = 1; i < argc; ++i) {
+            for (int y = 0; y < strlen(argv[i]); ++y) {
+                std::cout << (char)toupper(argv[i][y]);
+            }
+        }
+        std::cout << std::endl;
+    }
+    return 0;
+}
