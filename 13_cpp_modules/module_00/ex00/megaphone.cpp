59c63ae97f2eb7f94d0e2dec085a84a1e5fa21e9
Nicolas Gasco <nicolas.gasco92@gmail.com>
2022-04-21 15:50:54 +0200

    Refactored code

--- 13_cpp_modules/module_00/ex00/megaphone.cpp
+++ 13_cpp_modules/module_00/ex00/megaphone.cpp
@@ -1,16 +1,20 @@
-#include <iostream>
 #include <cstring>
+#include <iostream>
+
+void outputStringUpper(char *str) {
+    for (int y = 0; y < (int)strlen(str); ++y) {
+        std::cout << (char)toupper(str[y]);
+    }
+}
 
 int main(int argc, char *argv[]) {
-    if (argc == 1)
+    if (argc == 1) {
         std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-    else {
-        for (int i = 1; i < argc; ++i) {
-            for (int y = 0; y < strlen(argv[i]); ++y) {
-                std::cout << (char)toupper(argv[i][y]);
-            }
-        }
-        std::cout << std::endl;
+        return (0);
     }
+    for (int i = 1; i < argc; ++i) {
+        outputStringUpper(argv[i]);
+    }
+    std::cout << std::endl;
     return 0;
 }
