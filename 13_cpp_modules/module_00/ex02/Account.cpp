7b74a40c8f916ddecea6023fac2b50c63a8189f3
Nicolas Gasco <nicolas.gasco92@gmail.com>
2022-04-21 23:42:16 +0200

    Initialized project

---
+++ 13_cpp_modules/module_00/ex02/Account.cpp
@@ -0,0 +1,20 @@
+
+#include "Account.hpp";
+
+Account::Account(int initial_deposit) {
+}
+
+Account::~Account(void) {
+}
+
+int Account::getNbAccounts(void) {
+    return this->_nbAccounts;
+}
+int Account::getTotalAmount(void) {
+}
+int Account::getNbDeposits(void) {
+}
+int Account::getNbWithdrawals(void) {
+}
+void Account::displayAccountsInfos(void) {
+}
