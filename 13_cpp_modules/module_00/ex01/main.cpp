f9c4a4ad4c4b4d1ecc99fd650f3370106bef21b2
Nicolas Gasco <nicolas.gasco92@gmail.com>
2022-04-21 23:16:34 +0200

    Refactored code

--- 13_cpp_modules/module_00/ex01/main.cpp
+++ 13_cpp_modules/module_00/ex01/main.cpp
@@ -2,19 +2,13 @@
 
 #include "Phonebook.class.hpp"
 
-std::string colletCmdFromUser() {
-    std::string cmd;
-
-    std::cout << "Insert either ADD, SEARCH or EXIT: ";
-    std::getline(std::cin, cmd);
-    return (cmd);
-}
-
 int main(void) {
     PhoneBook phoneBook;
 
     while (1) {
-        std::string cmd = colletCmdFromUser();
+        std::string cmd;
+        std::cout << "Insert either ADD, SEARCH or EXIT: ";
+        std::getline(std::cin, cmd);
         if (cmd == "ADD") {
             phoneBook.addNewEntry();
         } else if (cmd == "SEARCH") {
