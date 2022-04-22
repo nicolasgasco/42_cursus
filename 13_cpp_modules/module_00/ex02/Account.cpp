7712a211660bb558b6a98bf7f0d3472e113a5dc9
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 <nicolas.gasco92@gmail.com>
2022-04-22 13:21:39 +0200

    Working on exercise 3

--- 13_cpp_modules/module_00/ex02/Account.cpp
+++ 13_cpp_modules/module_00/ex02/Account.cpp
@@ -1,20 +1,60 @@
 
-#include "Account.hpp";
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ngasco <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/22 13:14:49 by ngasco            #+#    #+#             */
+/*   Updated: 2022/04/22 13:14:58 by ngasco           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-Account::Account(int initial_deposit) {
+#include "Account.hpp"
+
+Account::Account(void) {
 }
 
 Account::~Account(void) {
 }
 
+// Getters
 int Account::getNbAccounts(void) {
-    return this->_nbAccounts;
+    return (0);
 }
 int Account::getTotalAmount(void) {
+    return (0);
 }
 int Account::getNbDeposits(void) {
+    return (0);
 }
 int Account::getNbWithdrawals(void) {
+    return (0);
 }
 void Account::displayAccountsInfos(void) {
+}
+
+// Deposit
+void Account::makeDeposit(int deposit) {
+    if (deposit) {
+        // 
+    }
+    return;
+}
+bool Account::makeWithdrawal(int withdrawal) {
+    if (withdrawal) {
+        //
+    }
+    return (false);
+}
+int Account::checkAmount(void) const {
+    return (0);
+}
+void Account::displayStatus(void) const {
+    return;
+}
+
+void Account::_displayTimestamp(void) {
+    return;
 }
