a713665042920f14ed080dd1d95993e8ef97f510
Nicolas Gasco <nicolas.gasco92@gmail.com>
2022-06-11 11:24:39 +0200

    Correct module_01/ex02

--- 14_cpp_modules/module_01/ex02/main.cpp
+++ 14_cpp_modules/module_01/ex02/main.cpp
@@ -1,18 +1,22 @@
 #include <iostream>
+#include <iomanip>
 
-int main(void) {
+#define STD_FORMAT std::setfill(' ') << std::setw(20) << std::left
+
+int main(void)
+{
     std::string myString = "HI THIS IS BRAIN";
-    std::string * stringPTR = &myString;
-    std::string & stringREF = myString;
+    std::string *stringPTR = &myString;
+    std::string &stringREF = myString;
 
-    std::cout << "String add: " << &myString << std::endl;
-    std::cout << "Pointer add: " << stringPTR << std::endl;
-    std::cout << "Reference add: " << &stringREF << std::endl;
+    std::cout << STD_FORMAT << "String address:" << &myString << std::endl;
+    std::cout << STD_FORMAT << "Pointer add: " << stringPTR << std::endl;
+    std::cout << STD_FORMAT << "Reference add: " << &stringREF << std::endl;
 
     std::cout << std::endl;
 
-    std::cout << "String value: " << myString << std::endl;
-    std::cout << "Pointer value: " << *stringPTR << std::endl;
-    std::cout << "Reference value: " << stringREF << std::endl;
+    std::cout << STD_FORMAT << "String value:" << myString << std::endl;
+    std::cout << STD_FORMAT << "Pointer value:" << *stringPTR << std::endl;
+    std::cout << STD_FORMAT << "Reference value:" << stringREF << std::endl;
     return (0);
 }
