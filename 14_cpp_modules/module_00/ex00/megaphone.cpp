616f247cf5f8cf12e8b3cd88372b03a5447504d1
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 <nicolas.gasco92@gmail.com>
2022-07-24 16:38:45 +0200

    Fix issue with ex00

--- 14_cpp_modules/module_00/ex00/megaphone.cpp
+++ 14_cpp_modules/module_00/ex00/megaphone.cpp
@@ -13,16 +13,15 @@
 #include <cstring>
 #include <iostream>
 
-int main(int argc, char *argv[])
-{
-    if (argc == 1)
-    {
+int main(int argc, char* argv[]) {
+    if (argc == 1) {
         std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
         return (0);
     }
-    for (int i = 1; i < argc; ++i)
-    {
-        std::cout << std::uppercase << argv[i];
+    for (int i = 1; i < argc; ++i) {
+        for (int x = 0; x < (int)strlen(argv[i]); ++x) {
+            std::cout << (char)toupper(argv[i][x]);
+        }
     }
     std::cout << std::endl;
     return 0;
