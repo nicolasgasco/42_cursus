a7b8b087f59e005457bc1cdb03452d203154be75
Nicolas Gasco <nicolas.gasco92@gmail.com>
2022-08-23 23:22:58 +0200

    Float to FPV conversion mostly done and working

--- 15_cpp_modules/module_02/ex01/Fixed.cpp
+++ 15_cpp_modules/module_02/ex01/Fixed.cpp
@@ -19,7 +19,6 @@ Fixed::Fixed(float const floatValue)
 {
     std::cout << "Float constructor called" << std::endl;
     this->_FixedPointValue = this->_floatToFixedPoint(floatValue);
-    std::cout << "Fixed point value is " << this->_FixedPointValue << std::endl;
 }
 
 Fixed::Fixed(Fixed const &src)
@@ -57,9 +56,8 @@ float Fixed::toFloat(void) const
 {
     float integralPart = this->toInt();
 
-    std::cout << "\nciao " << this->_FixedPointValue << std::endl;
     int decimalPart = (float)(this->_FixedPointValue & EIGHT_ACTIVE_BITS);
-    std::cout << "DecimalPart is " << decimalPart << std::endl;
+    // std::cout << "DecimalPart is " << decimalPart << std::endl;
     if (decimalPart == 0)
         return (integralPart);
 
@@ -67,7 +65,7 @@ float Fixed::toFloat(void) const
     for (int i = 1; i <= this->_NUM_FRACT_BITS; i++)
     {
         int bit = (decimalPart & (1 << (this->_NUM_FRACT_BITS - i))) >> (this->_NUM_FRACT_BITS - i);
-        std::cout << "Bit is " << (bit) << std::endl;
+        // std::cout << "Bit is " << (bit) << std::endl;
         float multiplier = 2.0f;
         for (int x = 0; x <= i; x++)
         {
@@ -91,60 +89,38 @@ int Fixed::_floatToFixedPoint(float const floatValue)
 
     int integralPart = (int)floatValue;
     int convertedIntegralPart = integralPart << this->_NUM_FRACT_BITS;
-    // Do something for integral part
-    std::cout << "ConvertedIntegralPart is " << convertedIntegralPart << std::endl;
+    // std::cout << "ConvertedIntegralPart is " << convertedIntegralPart << std::endl;
 
     floatValueCopy -= (int)(floatValueCopy);
-    std::cout << "floatValueCopy is " << floatValueCopy << std::endl;
+    // std::cout << "floatValueCopy is " << floatValueCopy << std::endl;
     float divider = 2.0f;
     for (int i = 0; i < this->_NUM_FRACT_BITS; i++)
     {
-        std::cout << "Bit position " << this->_NUM_FRACT_BITS - i << std::endl;
-        std::cout << "Divider is " << divider << std::endl;
+        // std::cout << "Bit position " << this->_NUM_FRACT_BITS - i << std::endl;
+        // std::cout << "Divider is " << divider << std::endl;
         float bitValue = (1.0 / divider);
-        std::cout
-            << "Bit value is " << bitValue << std::endl;
-        std::cout << "FloatValueCopy is " << floatValueCopy << std::endl;
+        // std::cout
+        //     << "Bit value is " << bitValue << std::endl;
+        // std::cout << "FloatValueCopy is " << floatValueCopy << std::endl;
         float divisionResult = (((floatValueCopy) / bitValue));
-        std::cout << "divisionResult is " << divisionResult << std::endl;
+        // std::cout << "divisionResult is " << divisionResult << std::endl;
         if (divisionResult < 1)
         {
-            std::cout << "Bit " << this->_NUM_FRACT_BITS - i << " is 0" << std::endl;
+            // std::cout << "Bit " << this->_NUM_FRACT_BITS - i << " is 0" << std::endl;
         }
         else
         {
-            std::cout << "Bit " << this->_NUM_FRACT_BITS - i << " is 1" << std::endl;
+            // std::cout << "Bit " << this->_NUM_FRACT_BITS - i << " is 1" << std::endl;
             int bitPositionInt = 1;
             for (int x = 1; x < (this->_NUM_FRACT_BITS - i); x++)
                 bitPositionInt *= 2;
-            std::cout << "convertedIntegralPart is " << convertedIntegralPart << std::endl;
+            // std::cout << "convertedIntegralPart is " << convertedIntegralPart << std::endl;
             convertedIntegralPart = convertedIntegralPart | bitPositionInt;
-            std::cout << "convertedIntegralPart is " << convertedIntegralPart << std::endl;
+            // std::cout << "convertedIntegralPart is " << convertedIntegralPart << std::endl;
             floatValueCopy -= bitValue;
         }
-        std::cout << std::endl;
         divider *= 2.0f;
     }
-
-    std::cout << std::endl;
-
-    int numDecimals = 0;
-    while (floatValueCopy != roundf(floatValueCopy))
-    {
-        floatValueCopy *= 10.0;
-        integralPart *= 10;
-        numDecimals++;
-    }
-
-    int convertedFloatPart = (int)(floatValueCopy - integralPart);
-    // For numbers with higher decimal precision that 8 bits
-    while (convertedFloatPart > EIGHT_ACTIVE_BITS)
-    {
-        convertedFloatPart /= 10;
-        numDecimals--;
-    }
-
-    this->_numOfDecimals = numDecimals;
     return (convertedIntegralPart);
 }
 
--- 15_cpp_modules/module_02/ex01/main.cpp
+++ 15_cpp_modules/module_02/ex01/main.cpp
@@ -3,27 +3,57 @@
 
 int main(void)
 {
-    // Fixed a;
-    // Fixed const b(10);
-    Fixed const c(0.0124324f);
-    // Fixed const d(b);
+    Fixed a;
+    Fixed const b(10);
+    Fixed const c(42.43f); // FPV should be 10862
+    Fixed const d(b);
 
-    // a = Fixed(1234.4321f);
+    a = Fixed(1234.4321f); // FPV should be 316015
+
+    // More float examples
+    Fixed const e(24885.438943f); // FVP should be 6370672
+    Fixed const f(0.0f);          // FVP should be 0
+    Fixed const g(0.025f);        // FVP should be 6
+    Fixed const h(33.3333f);      // FVP should be 8533
+    Fixed const i(12345.54321f);  // FVP should be 3160459
+    Fixed const l(0.99f);         // FVP should be 253
 
     std::cout << std::endl;
 
-    // std::cout << "a is " << a << std::endl;
-    // std::cout << "b is " << b << std::endl;
+    std::cout << "a is " << a << std::endl;
+    std::cout << "b is " << b << std::endl;
     std::cout << "c is " << c << std::endl;
-    // std::cout << "d is " << d << std::endl;
+    std::cout << "d is " << d << std::endl;
 
     std::cout << std::endl;
 
-    // std::cout << "a is " << a.toInt() << " as integer" << std::endl;
-    // std::cout << "b is " << b.toInt() << " as integer" << std::endl;
+    std::cout << "e is " << e << std::endl;
+    std::cout << "f is " << f << std::endl;
+    std::cout << "g is " << g << std::endl;
+    std::cout << "h is " << h << std::endl;
+    std::cout << "i is " << i << std::endl;
+    std::cout << "l is " << l << std::endl;
+
+    std::cout << std::endl;
+
+    std::cout
+        << "a is " << a.toInt() << " as integer" << std::endl;
+    std::cout << "b is " << b.toInt() << " as integer" << std::endl;
     std::cout << "c is " << c.toInt() << " as integer" << std::endl;
-    // std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+    std::cout << "d is " << d.toInt() << " as integer" << std::endl;
 
     std::cout << std::endl;
+
+    std::cout << std::endl;
+
+    std::cout << "e is " << e.toInt() << " as integer" << std::endl;
+    std::cout << "f is " << f.toInt() << " as integer" << std::endl;
+    std::cout << "g is " << g.toInt() << " as integer" << std::endl;
+    std::cout << "h is " << h.toInt() << " as integer" << std::endl;
+    std::cout << "i is " << i.toInt() << " as integer" << std::endl;
+    std::cout << "l is " << l.toInt() << " as integer" << std::endl;
+
+    std::cout << std::endl;
+
     return 0;
 }
