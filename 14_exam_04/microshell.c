b0827c39fb0f8bb3bf0e25e7af736cb6d6a778de
Nicolas Gasco <nicolas.gasco92@gmail.com>
2022-08-23 22:42:26 +0200

    Practice one more time with vim

--- 14_exam_04/microshell.c
+++ 14_exam_04/microshell.c
@@ -1,39 +1,27 @@
-#include <unistd.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <stdio.h>
 #include <string.h>
 
-void ft_write_fd(int fd, char *str)
+void	ft_write_fd(int fd, char *str)
 {
 	int i = 0;
 	while (str[i] != '\0')
 	{
-		write(fd, &str[i], sizeof(char));
+		write(fd, &str[i], sizeof(char)); 
 		i++;
 	}
 }
 
-void ft_fatal_error(void)
+void	ft_fatal_error(void)
 {
 	ft_write_fd(STDERR_FILENO, "error: fatal\n");
-	exit(2);
+	exit(1);
 }
 
-int ft_get_num_args(char **argv)
+void	ft_get_trimmed_args(char **argv, char **trimmed_args)
 {
-	int i = 0;
-	while (*(argv + i))
-	{
-		if (strcmp(*(argv + i), "|") == 0 || strcmp(*(argv + i), ";") == 0)
-			break;
-		i++;
-	}
-	return (i);
-}
-
-void ft_get_trimmed_args(char **trimmed_args, char **argv)
-{
-	int i = 0;
+	int	i = 0;
 	while (*(argv + i))
 	{
 		if (strcmp(*(argv + i), "|") == 0 || strcmp(*(argv + i), ";") == 0)
@@ -44,29 +32,42 @@ void ft_get_trimmed_args(char **trimmed_args, char **argv)
 	trimmed_args[i] = NULL;
 }
 
-void ft_cd(char **argv)
+int	ft_get_num_args(char **argv)
 {
-	argv++;
+	int	i = 0;
+	while (*(argv + i))
+	{
+		if (strcmp(*(argv + i), ";") == 0 || strcmp(*(argv + i), "|") == 0)
+			break;
+		i++;
+	}
+	return(i);
+}
+
+void	ft_cd(char **argv)
+{	
 	int num_args = ft_get_num_args(argv);
-	if (num_args != 1)
+	if (num_args != 2)
 	{
 		ft_write_fd(STDERR_FILENO, "error: cd: bad arguments\n");
-		exit(2);
-	}
+		exit(1);
+	}	
+	argv++;
 	if (chdir(*argv) == -1)
 	{
 		ft_write_fd(STDERR_FILENO, "error: cd: cannot change directory to ");
 		ft_write_fd(STDERR_FILENO, *argv);
 		ft_write_fd(STDERR_FILENO, "\n");
-		exit(2);
+		exit(1);
 	}
 	argv++;
 }
 
-int main(int argc, char **argv, char **envp)
+int	main(int argc, char **argv, char **envp)
 {
-	int fd[2][2];
-	int pipe_opened = 0;
+	int	fd[2][2];
+	int	pipe_opened = 0;
+
 	fd[0][0] = -1;
 	fd[0][1] = -1;
 	fd[1][0] = -1;
@@ -74,33 +75,33 @@ int main(int argc, char **argv, char **envp)
 	argv++;
 	while (*argv)
 	{
-		int must_read = 0;
-		int must_write = 0;
-		int must_write_read = 0;
+		int	must_read = 0;
+		int	must_write = 0;
+		int	must_write_read = 0;
 		if (strcmp(*argv, ";") == 0)
+			argv++;
+		else if (strcmp(*argv, "|") == 0)
 			argv++;
 		else if (strcmp(*argv, "cd") == 0)
 		{
 			ft_cd(argv);
-			argv += ft_get_num_args(argv);
+			argv += ft_get_num_args(argv);	
 		}
-		else if (strcmp(*argv, "|") == 0)
-			argv++;
 		else
 		{
 			int num_args = ft_get_num_args(argv);
 			if (*(argv + num_args) && strcmp(*(argv + num_args), "|") == 0)
 			{
-				int fd_num;
+				int	fd_num ;
 				if (pipe_opened == 0)
 				{
-					fd_num = 0;
 					must_write = 1;
+					fd_num = 0;
 				}
 				else
 				{
-					fd_num = 1;
 					must_write_read = 1;
+					fd_num = 1;
 				}
 				if (pipe(fd[fd_num]) == -1)
 					ft_fatal_error();
@@ -109,11 +110,7 @@ int main(int argc, char **argv, char **envp)
 			else
 			{
 				if (pipe_opened == 1)
-				{
 					must_read = 1;
-				}
-				else
-					pipe_opened = 0;
 			}
 			int pid = fork();
 			if (pid < 0)
@@ -122,30 +119,30 @@ int main(int argc, char **argv, char **envp)
 			{
 				if (must_write == 1)
 				{
-					if (dup2(fd[0][1], STDOUT_FILENO) < 0)
+					if (dup2(fd[0][1], STDOUT_FILENO) == -1)
 						ft_fatal_error();
 				}
 				else if (must_read == 1)
 				{
-					if (dup2(fd[0][0], STDIN_FILENO) < 0)
+					if (dup2(fd[0][0], STDIN_FILENO) == -1)
 						ft_fatal_error();
 				}
 				else if (must_write_read == 1)
 				{
-					if (dup2(fd[0][0], STDIN_FILENO) < 0)
+					if (dup2(fd[1][1], STDOUT_FILENO) == -1)
 						ft_fatal_error();
-					if (dup2(fd[1][1], STDOUT_FILENO) < 0)
-						ft_fatal_error();
+					if (dup2(fd[0][0], STDIN_FILENO) == -1)
+						ft_fatal_error();	
 				}
 				char *trimmed_args[argc];
-				ft_get_trimmed_args(trimmed_args, argv);
+				ft_get_trimmed_args(argv, trimmed_args);
 				if (execve(*argv, trimmed_args, envp) == -1)
 				{
 					ft_write_fd(STDERR_FILENO, "error: cannot execute ");
 					ft_write_fd(STDERR_FILENO, *argv);
 					ft_write_fd(STDERR_FILENO, "\n");
+					exit(1);
 				}
-				exit(2);
 			}
 			else
 			{
@@ -164,11 +161,12 @@ int main(int argc, char **argv, char **envp)
 					close(fd[1][1]);
 					fd[1][1] = -1;
 				}
-				argv += ft_get_num_args(argv);
+
+				argv += num_args;
 			}
-		}
+		}	
 	}
-	if (fd[0][0] != -1 && fd[1][0] == -1)
+	if (fd[0][0] != -1 && fd[0][1] == -1)
 		close(fd[0][0]);
 	if (fd[1][0] != -1)
 		close(fd[1][0]);
--- 14_exam_04/microshell_0.c
+++ 14_exam_04/microshell_0.c
@@ -1,8 +1,7 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <errno.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
 void ft_write_fd(int fd, char *str)
 {
@@ -14,119 +13,103 @@ void ft_write_fd(int fd, char *str)
 	}
 }
 
-void ft_execve_error(char **argv)
-{
-	ft_write_fd(STDERR_FILENO, "error: cannot execute ");
-	ft_write_fd(STDERR_FILENO, *argv);
-	ft_write_fd(STDERR_FILENO, "\n");
-	exit(1);
-}
-
 void ft_fatal_error(void)
 {
 	ft_write_fd(STDERR_FILENO, "error: fatal\n");
-	exit(1);
-}
-
-void ft_trim_args(char **argv, char **new_args)
-{
-	int i;
-	for (i = 0; argv[i]; i++)
-	{
-		new_args[i] = argv[i];
-		if (strcmp(argv[i], "|") == 0 || strcmp(argv[i], ";") == 0)
-			break;
-	}
-	new_args[i] = NULL;
+	exit(2);
 }
 
 int ft_get_num_args(char **argv)
 {
-	int i;
-	for (i = 0; argv[i]; i++)
+	int i = 0;
+	while (*(argv + i))
 	{
-		if (strcmp(argv[i], "|") == 0 || strcmp(argv[i], ";") == 0)
-			return (i);
+		if (strcmp(*(argv + i), "|") == 0 || strcmp(*(argv + i), ";") == 0)
+			break;
+		i++;
 	}
 	return (i);
 }
 
+void ft_get_trimmed_args(char **trimmed_args, char **argv)
+{
+	int i = 0;
+	while (*(argv + i))
+	{
+		if (strcmp(*(argv + i), "|") == 0 || strcmp(*(argv + i), ";") == 0)
+			break;
+		trimmed_args[i] = *(argv + i);
+		i++;
+	}
+	trimmed_args[i] = NULL;
+}
+
 void ft_cd(char **argv)
 {
+	argv++;
 	int num_args = ft_get_num_args(argv);
-	if (num_args != 2)
+	if (num_args != 1)
 	{
 		ft_write_fd(STDERR_FILENO, "error: cd: bad arguments\n");
-		exit(1);
+		exit(2);
 	}
-	argv++;
 	if (chdir(*argv) == -1)
 	{
 		ft_write_fd(STDERR_FILENO, "error: cd: cannot change directory to ");
 		ft_write_fd(STDERR_FILENO, *argv);
 		ft_write_fd(STDERR_FILENO, "\n");
-		exit(1);
+		exit(2);
 	}
 	argv++;
 }
 
-int main(int argc, char *argv[], char **envp)
+int main(int argc, char **argv, char **envp)
 {
 	int fd[2][2];
+	int pipe_opened = 0;
 	fd[0][0] = -1;
 	fd[0][1] = -1;
 	fd[1][0] = -1;
 	fd[1][1] = -1;
-	int num_args;
-	int pipe_opened = 0;
-
 	argv++;
 	while (*argv)
 	{
-		int must_write = 0;
 		int must_read = 0;
+		int must_write = 0;
 		int must_write_read = 0;
-		if (strcmp(*argv, "|") == 0 && *(argv + 1))
-		{
-			argv += 1;
-		}
 		if (strcmp(*argv, ";") == 0)
-		{
-			argv += 1;
-		}
+			argv++;
 		else if (strcmp(*argv, "cd") == 0)
 		{
 			ft_cd(argv);
 			argv += ft_get_num_args(argv);
 		}
+		else if (strcmp(*argv, "|") == 0)
+			argv++;
 		else
 		{
-			num_args = ft_get_num_args(argv);
+			int num_args = ft_get_num_args(argv);
 			if (*(argv + num_args) && strcmp(*(argv + num_args), "|") == 0)
 			{
-				int pipe_num;
+				int fd_num;
 				if (pipe_opened == 0)
 				{
-					// printf("Opening first pipe for writing only\n");
+					fd_num = 0;
 					must_write = 1;
-					pipe_num = 0;
 				}
 				else
 				{
-					// printf("Opening second pipe for reading and writing\n");
+					fd_num = 1;
 					must_write_read = 1;
-					pipe_num = 1;
 				}
-				if (pipe(fd[pipe_num]) == -1)
+				if (pipe(fd[fd_num]) == -1)
 					ft_fatal_error();
 				pipe_opened = 1;
 			}
 			else
 			{
-				// printf("Next one is not a pipe (%s)\n", *argv);
 				if (pipe_opened == 1)
 				{
-					// printf("Not opening pipe, only reading\n");
 					must_read = 1;
 				}
 				else
@@ -139,29 +122,30 @@ int main(int argc, char *argv[], char **envp)
 			{
 				if (must_write == 1)
 				{
-					// printf("Writing into pipe\n");
 					if (dup2(fd[0][1], STDOUT_FILENO) < 0)
 						ft_fatal_error();
 				}
 				else if (must_read == 1)
 				{
-					// printf("Reading into pipe\n");
 					if (dup2(fd[0][0], STDIN_FILENO) < 0)
 						ft_fatal_error();
 				}
 				else if (must_write_read == 1)
 				{
-					// printf("Reading from and writing into pipe\n");
 					if (dup2(fd[0][0], STDIN_FILENO) < 0)
 						ft_fatal_error();
 					if (dup2(fd[1][1], STDOUT_FILENO) < 0)
 						ft_fatal_error();
 				}
-				char *new_args[argc];
-				ft_trim_args(argv, new_args);
-				if (execve(*argv, new_args, envp) == -1)
-					ft_execve_error(argv);
-				exit(1);
+				char *trimmed_args[argc];
+				ft_get_trimmed_args(trimmed_args, argv);
+				if (execve(*argv, trimmed_args, envp) == -1)
+				{
+					ft_write_fd(STDERR_FILENO, "error: cannot execute ");
+					ft_write_fd(STDERR_FILENO, *argv);
+					ft_write_fd(STDERR_FILENO, "\n");
+				}
+				exit(2);
 			}
 			else
 			{
@@ -180,21 +164,14 @@ int main(int argc, char *argv[], char **envp)
 					close(fd[1][1]);
 					fd[1][1] = -1;
 				}
-				num_args = ft_get_num_args(argv);
-				argv += num_args;
+				argv += ft_get_num_args(argv);
 			}
 		}
 	}
 	if (fd[0][0] != -1 && fd[1][0] == -1)
-	{
 		close(fd[0][0]);
-		fd[0][0] = -1;
-	}
 	if (fd[1][0] != -1)
-	{
 		close(fd[1][0]);
-		fd[1][0] = -1;
-	}
 	system("lsof -c microshell | grep PIPE");
 	return (0);
 }
