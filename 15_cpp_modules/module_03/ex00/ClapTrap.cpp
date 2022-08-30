2b21798ec608497778fe440c0f1a2ce7c7bfe8a1
Nicolas Gasco <nicolas.gasco92@gmail.com>
2022-08-30 23:25:18 +0200

    Make consistent with 00

--- 15_cpp_modules/module_03/ex00/ClapTrap.cpp
+++ 15_cpp_modules/module_03/ex00/ClapTrap.cpp
@@ -2,18 +2,18 @@
 
 ClapTrap::ClapTrap(void) : _hitpoints(10), _energyPoints(10), _attackDamage(0)
 {
-    std::cout << "Default constructor called" << std::endl;
+    std::cout << "ClapTrap default constructor called" << std::endl;
 }
 
 ClapTrap::ClapTrap(std::string name) : _hitpoints(10), _energyPoints(10), _attackDamage(0)
 {
     this->_name = name;
-    std::cout << "Paramter constructor called (" << name << ")" << std::endl;
+    std::cout << "ClapTrap paramter constructor called (" << name << ")" << std::endl;
 }
 
 ClapTrap::~ClapTrap(void)
 {
-    std::cout << "Destructor called" << std::endl;
+    std::cout << "ClapTrap destructor called" << std::endl;
 }
 
 // Assignment operator
@@ -28,26 +28,18 @@ void ClapTrap::attack(std::string const &target)
 {
     this->_energyPoints--;
     if (this->_name.length())
-    {
         std::cout << "ClapTrap " << this->_name << " attacks " << target << ", causing " << this->_attackDamage << " points of damage!" << std::endl;
-    }
     else
-    {
         std::cout << "Unnamed ClapTrap attacks " << target << ", causing " << this->_attackDamage << " points of damage!" << std::endl;
-    }
 }
 
 void ClapTrap::takeDamage(unsigned int amount)
 {
     this->_hitpoints -= amount;
     if (this->_name.length())
-    {
         std::cout << "ClapTrap " << this->_name << " takes " << amount << " points of damage!" << std::endl;
-    }
     else
-    {
         std::cout << "Unnamed ClapTrap takes " << amount << " points of damage!" << std::endl;
-    }
 }
 
 void ClapTrap::beRepaired(unsigned int amount)
@@ -80,13 +72,9 @@ int ClapTrap::getAttackDamage(void) const
 std::ostream &operator<<(std::ostream &os, ClapTrap const &std)
 {
     if (std.getName().length())
-    {
         std::cout << "Name: " << std.getName() << std::endl;
-    }
     else
-    {
-        std::cout << "No name" << std::endl;
-    }
+        std::cout << "Name: n/a" << std::endl;
     std::cout << "Hit points: " << std.getHitPoints() << std::endl;
     std::cout << "Damage points: " << std.getAttackDamage() << std::endl;
     std::cout << "Energy points: " << std.getEnergyPoints() << std::endl;
