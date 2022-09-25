15721ed3b703c2aa48386093da7284569248b7a3
Nicolas Gasco <nicolas.gasco92@gmail.com>
2022-09-25 22:30:10 +0200

    Add new checks to module_04/ex02

--- 15_cpp_modules/module_04/ex02/main.cpp
+++ 15_cpp_modules/module_04/ex02/main.cpp
@@ -31,6 +31,37 @@ int main(void)
     delete i;
     std::cout << std::endl;
 
+    std::cout << "Deep copy tests:" << std::endl;
+    Cat seed;
+    std::cout << std::endl;
+
+    Cat copy = Cat(seed);
+    std::cout << std::endl;
+
+    Cat copy2 = seed;
+    std::cout << std::endl;
+
+    std::cout << "Brains memory addresses:" << std::endl;
+    std::cout << seed.getBrain() << std::endl;
+    std::cout << copy.getBrain() << std::endl;
+    std::cout << copy2.getBrain() << std::endl;
+    std::cout << std::endl;
+
+    Dog seedDog;
+    std::cout << std::endl;
+
+    Dog copyDog = Dog(seedDog);
+    std::cout << std::endl;
+
+    Dog copy2Dog = seedDog;
+    std::cout << std::endl;
+
+    std::cout << "Brains memory addresses:" << std::endl;
+    std::cout << seedDog.getBrain() << std::endl;
+    std::cout << copyDog.getBrain() << std::endl;
+    std::cout << copy2Dog.getBrain() << std::endl;
+    std::cout << std::endl;
+
     // const Animal *wrong = new Animal();
     return (0);
 }
