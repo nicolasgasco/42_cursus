fbe84f44130784f5b4a483c95abbb796182d1ba3
Nicolas Gasco <nicolas.gasco92@gmail.com>
2022-09-26 23:25:33 +0200

    Complete module_06/ex00

--- 15_cpp_modules/module_06/ex00/main.cpp
+++ 15_cpp_modules/module_06/ex00/main.cpp
@@ -1,52 +1,83 @@
 #include <iostream>
 
-int main(int argc, char *argv[]) {
-    if (argc != 2) {
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
         std::cerr << "Error: wrong arguments" << std::endl;
         return (1);
     }
 
     std::string strArg = argv[1];
 
+    // INT
     std::cout << "int: ";
-    try {
+    try
+    {
+        int intArg = std::stoi(strArg);
+        std::cout << intArg << std::endl;
+    }
+    catch (...)
+    {
+        std::cout << "impossible" << std::endl;
+    }
+
+    // CHAR
+    std::cout << "char: ";
+    try
+    {
         int intArg = std::stoi(strArg);
         std::cout << intArg << std::endl;
 
         std::cout << "char: ";
-        if (intArg <= 31) {
+        if (intArg <= 31)
+        {
             std::cout << "non displayable" << std::endl;
-        } else if (intArg > 127) {
+        }
+        else if (intArg > 127)
+        {
             std::cout << "impossible" << std::endl;
-        } else {
+        }
+        else
+        {
             char charArg = static_cast<char>(intArg);
             std::cout << "'" << charArg << "'" << std::endl;
         }
-    } catch (...) {
-        std::cout << "impossible" << std::endl;
-
-        if (strArg.length() == 1) {
+    }
+    catch (...)
+    {
+        if (strArg.length() == 1)
+        {
             char charArg = static_cast<char>(strArg.at(0));
             std::cout << "char: '" << charArg << "'" << std::endl;
-        } else {
+        }
+        else
+        {
             std::cout << "char: imposible" << std::endl;
         }
     }
 
+    // FLOAT
     std::cout << "float: ";
-    try {
+    try
+    {
         float floatArg = std::stof(strArg);
         std::cout << floatArg << std::endl;
-
-    } catch (...) {
+    }
+    catch (...)
+    {
         std::cout << "impossible" << std::endl;
     }
 
+    // DOUBLE
     std::cout << "double: ";
-    try {
+    try
+    {
         double doubleArg = std::stod(strArg);
         std::cout << doubleArg << std::endl;
-    } catch (...) {
+    }
+    catch (...)
+    {
         std::cout << "impossible" << std::endl;
     }
     return (0);
