804d9029fe1ea5689e9252a00bc5e8050ed7cd5d
Nicolas Gasco <nicolas.gasco92@gmail.com>
2022-09-27 19:10:48 +0200

    Complete module_06/ex02

--- 15_cpp_modules/module_06/ex02/main.cpp
+++ 15_cpp_modules/module_06/ex02/main.cpp
@@ -44,6 +44,44 @@ void identify(Base *p)
         std::cout << "Unknown pointer" << std::endl;
 }
 
+void identify(Base &p)
+{
+    // A
+    try
+    {
+        A &result = dynamic_cast<A &>(p);
+        std::cout << "Pointer is A (" << &result << ")" << std::endl;
+        return;
+    }
+    catch (...)
+    {
+    }
+
+    // B
+    try
+    {
+        B &result = dynamic_cast<B &>(p);
+        std::cout << "Pointer is B (" << &result << ")" << std::endl;
+        return;
+    }
+    catch (...)
+    {
+    }
+
+    // C
+    try
+    {
+        C &result = dynamic_cast<C &>(p);
+        std::cout << "Pointer is C (" << &result << ")" << std::endl;
+        return;
+    }
+    catch (...)
+    {
+    }
+
+    // std::cout << "Pointer is unknown" << std::endl;
+}
+
 int main(void)
 {
     std::srand(time(NULL));
@@ -54,10 +92,12 @@ int main(void)
 
         identify(pointer);
         std::cout << std::endl;
-        std::cout << "_______________________\n"
-                  << std::endl;
+
+        identify(*pointer);
 
         delete pointer;
+        std::cout << "_______________________\n"
+                  << std::endl;
     }
 
     return (0);
