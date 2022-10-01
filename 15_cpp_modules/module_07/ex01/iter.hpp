dd1c32acebafc5232c0d3419a1e19a8462ac7b94
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 <nicolas.gasco92@gmail.com>
2022-10-01 18:00:37 +0200

    Complete module_07/ex01

--- 15_cpp_modules/module_07/ex01/iter.hpp
+++ 15_cpp_modules/module_07/ex01/iter.hpp
@@ -2,9 +2,16 @@
 #ifndef ITER_HPP
 #define ITER_HPP
 
-template <typename T, L, >
-void iter(T a, L b, Tc)
-{
+template <typename T>
+void iter(T *a, unsigned int len, void func(T &)) {
+    for (unsigned int i = 0; i < len; i++) {
+        func(a[i]);
+    }
+}
+
+template <typename T>
+void outputArrayValue(T value) {
+    std::cout << "- " << value << std::endl;
 }
 
 #endif
--- 15_cpp_modules/module_07/ex01/main.cpp
+++ 15_cpp_modules/module_07/ex01/main.cpp
@@ -1,4 +1,56 @@
-int main(void)
-{
+#include <iostream>
+#include <string>
+
+#include "iter.hpp"
+
+void outputToUpper(std::string &str) {
+    std::cout << "- ";
+    for (unsigned int i = 0; i < str.length(); i++) {
+        std::cout << static_cast<char>(toupper(str[i]));
+    }
+    std::cout << std::endl;
+}
+
+int main(void) {
+    std::cout << "STRING ARRAY:" << std::endl;
+
+    std::string strArr[] = {"ciao", "hola", "hallo"};
+
+    std::cout << "Apply template function:" << std::endl;
+    iter(strArr, sizeof(strArr) / sizeof(std::string), outputArrayValue);
+    std::cout << std::endl;
+
+    std::cout << "Apply outPutToUpper:" << std::endl;
+    iter(strArr, sizeof(strArr) / sizeof(std::string), outputToUpper);
+    std::cout << std::endl;
+
+    std::cout << std::endl;
+    std::cout << "INT ARRAY:" << std::endl;
+
+    int intArr[] = {2, 3, 4};
+
+    iter(intArr, sizeof(intArr) / sizeof(int), outputArrayValue);
+
+    std::cout << std::endl;
+    std::cout << "FLOAT ARRAY:" << std::endl;
+
+    float floatArr[] = {2.1f, 3.4f, 4.7f};
+
+    iter(floatArr, sizeof(floatArr) / sizeof(float), outputArrayValue);
+
+    std::cout << std::endl;
+    std::cout << "DOUBLE ARRAY:" << std::endl;
+
+    double doubleArray[] = {253445234545, 234542352345, 6787867868};
+
+    iter(doubleArray, sizeof(doubleArray) / sizeof(double), outputArrayValue);
+
+    std::cout << std::endl;
+    std::cout << "CHAR ARRAY:" << std::endl;
+
+    char charArray[] = {'a', 'b', 'c'};
+
+    iter(charArray, sizeof(charArray) / sizeof(char), outputArrayValue);
+
     return (0);
 }
