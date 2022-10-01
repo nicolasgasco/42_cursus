cda7b7d51a705ca4895504a06bfb02c01cd4d929
Nicolas Gasco <nicolas.gasco92@gmail.com>
2022-10-01 21:35:43 +0200

    Complete module_07/ex02

--- 15_cpp_modules/module_07/ex02/Array.hpp
+++ 15_cpp_modules/module_07/ex02/Array.hpp
@@ -6,19 +6,87 @@
 #define NOCOL "\033[0m"
 
 template <typename T>
-class Array {
-   public:
-    Array(void) {
-        values = {};
+class Array
+{
+public:
+    Array<T>(unsigned int n)
+    {
+        std::cout << PURPLE << "New array (parameter constructor)" << NOCOL << std::endl;
+        this->arrSize = n;
+        this->values = new T[n];
     }
-    Array(Array const &src);
+    Array<T>(void)
+    {
+        std::cout << PURPLE << "New array (default constructor)" << NOCOL << std::endl;
+        this->arrSize = 0;
+        this->values = new T[0];
+    }
+    Array<T>(Array<T> const &src)
+    {
+        std::cout << PURPLE << "New array (copy constructor)" << NOCOL << std::endl;
+        *this = src;
+    }
 
-    ~Array(void);
+    ~Array<T>(void)
+    {
+        std::cout << PURPLE << "Array destructor" << NOCOL << std::endl;
+    }
 
-    Array &operator=(Array const &src);
+    Array<T> &operator=(Array<T> const &src)
+    {
+        this->arrSize = src.arrSize;
+        this->values = new T[arrSize];
+        for (unsigned int i = 0; i < arrSize; i++)
+        {
+            this->values[i] = src.values[i];
+        }
+        return (*this);
+    }
 
-   private:
-    T values[];
+    T &operator[](int index)
+    {
+        if (index < 0 || index > (static_cast<int>(arrSize) - 1))
+            throw Array<T>::IndexOutOfBound();
+        return (this->values[index]);
+    }
+
+    unsigned int size(void) const
+    {
+        return arrSize;
+    }
+
+private:
+    T *values;
+    unsigned int arrSize;
+
+    class IndexOutOfBound : public std::exception
+    {
+    public:
+        virtual const char *what() const throw()
+        {
+            return ("Index value out of bound");
+        }
+    };
 };
+
+template <typename T>
+std::ostream &operator<<(std::ostream &os, Array<T> const &std)
+{
+    std::cout << PURPLE << "--------------------------------------------------------------------------------------------------------" << std::endl;
+    std::cout << "Size of array is: " << std.size() << std::endl;
+
+    if (std.size() == 0)
+        std::cout << "* empty *" << std::endl;
+
+    for (unsigned int i = 0; i < ((std.size() < 50) ? std.size() : 50); i++)
+    {
+        std::cout << const_cast<Array<T> &>(std)[i] << (i == (std.size() - 1) ? "" : "  ");
+    }
+    if (std.size() > 50)
+        std::cout << "... ... ... (results hidden for brevity) ... ... ..." << std::endl;
+    std::cout << std::endl;
+    std::cout << "--------------------------------------------------------------------------------------------------------" << NOCOL << std::endl;
+    return os;
+}
 
 #endif
--- 15_cpp_modules/module_07/ex02/main.cpp
+++ 15_cpp_modules/module_07/ex02/main.cpp
@@ -3,7 +3,146 @@
 
 #include "Array.hpp"
 
-int main(void) {
-    Array array;
+#define MAX_VAL 750
+
+int main(void)
+{
+    std::cout << "New array with default constructor" << std::endl;
+    Array<int> array;
+    std::cout << std::endl;
+
+    std::cout << "Outputting empty array:" << std::endl;
+    std::cout << array << std::endl;
+    std::cout << std::endl;
+
+    std::cout << "Accessing index -1" << std::endl;
+    try
+    {
+        std::cout << "Value of index -1 is: " << array[-1] << std::endl;
+    }
+    catch (std::exception &e)
+    {
+        std::cout << "Error: " << e.what() << std::endl;
+        std::cout << std::endl;
+    }
+
+    std::cout << "Accessing index 1" << std::endl;
+    try
+    {
+        std::cout << "Value of index 1 is: " << array[1] << std::endl;
+    }
+    catch (std::exception &e)
+    {
+        std::cout << "Error: " << e.what() << std::endl;
+        std::cout << std::endl;
+    }
+
+    std::cout << "New array with parameter constructor (10)" << std::endl;
+    Array<int> array2(10);
+    std::cout << std::endl;
+
+    std::cout << "Filling in array with sequential numbers" << std::endl;
+    for (unsigned int i = 0; i < array2.size(); i++)
+        array2[i] = i + 1;
+
+    std::cout << "Outputting filled array:" << std::endl;
+    std::cout << array2 << std::endl;
+    std::cout << std::endl;
+
+    std::cout << "Accessing index 0" << std::endl;
+    try
+    {
+        std::cout << "Value of index 0 is: " << array2[0] << std::endl;
+        std::cout << std::endl;
+    }
+    catch (std::exception &e)
+    {
+        std::cout << "Error: " << e.what() << std::endl;
+        std::cout << std::endl;
+    }
+
+    std::cout << "Accessing index 9" << std::endl;
+    try
+    {
+        std::cout << "Value of index 9 is: " << array2[9] << std::endl;
+        std::cout << std::endl;
+    }
+    catch (std::exception &e)
+    {
+        std::cout << "Error: " << e.what() << std::endl;
+        std::cout << std::endl;
+    }
+
+    std::cout << "Accessing index 10" << std::endl;
+    try
+    {
+        std::cout << "Value of index 10 is: " << array2[10] << std::endl;
+        std::cout << std::endl;
+    }
+    catch (std::exception &e)
+    {
+        std::cout << "Error: " << e.what() << std::endl;
+        std::cout << std::endl;
+    }
+
+    // PROVIDED MAIN
+    std::cout << "42 PROVIDED MAIN" << std::endl;
+
+    Array<int>
+        numbers(MAX_VAL);
+    int *mirror = new int[MAX_VAL];
+    srand(time(NULL));
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        const int value = rand();
+        numbers[i] = value;
+        mirror[i] = value;
+    }
+    std::cout << "Outputting numbers:" << std::endl;
+    std::cout << numbers << std::endl;
+
+    // SCOPE
+    {
+        Array<int> tmp = numbers;
+        Array<int> test(tmp);
+
+        std::cout << "Outputting tmp:" << std::endl;
+        std::cout << tmp << std::endl;
+
+        std::cout << "Outputting test:" << std::endl;
+        std::cout << test << std::endl;
+    }
+
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        if (mirror[i] != numbers[i])
+        {
+            std::cerr << "didn't save the same value!!" << std::endl;
+            return 1;
+        }
+    }
+    try
+    {
+        numbers[-2] = 0;
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    try
+    {
+        numbers[MAX_VAL] = 0;
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        numbers[i] = rand();
+    }
+    delete[] mirror; //
+    return 0;
     return (0);
 }
