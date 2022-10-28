c5ac2ab3ff26ce08630b9804b8509660d15e3292
Nicolas Gasco <nicolas.gasco92@gmail.com>
2022-10-28 19:05:05 +0200

    Review module_08/ex00

--- 15_cpp_modules/module_08/ex00/main.cpp
+++ 15_cpp_modules/module_08/ex00/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <iterator>
-#include <list>
+#include <vector>
 #include <iomanip>
 
 #include "easyfind.hpp"
@@ -13,14 +13,14 @@ int main(void)
 {
     std::cout << YELLOW << "LIST OF INT:" << NOCOL << std::endl;
     std::cout << "Creating a list of ints and filling it up with random values between 1 and 10: " << std::endl;
-    std::list<int> myList;
+    std::vector<int> myList;
     srand(time(0));
     for (int i = 0; i < 10; ++i)
         myList.push_back(rand() % 10 + 1);
 
     std::cout << "Printing list using iterators" << std::endl;
-    std::list<int>::iterator it = myList.begin();
-    std::list<int>::iterator ite = myList.end();
+    std::vector<int>::iterator it = myList.begin();
+    std::vector<int>::iterator ite = myList.end();
     for (; it != ite; ++it)
         std::cout << std::right << " " << *it << "\t";
     std::cout << std::endl;
@@ -45,14 +45,14 @@ int main(void)
     std::cout << std::endl;
     std::cout << YELLOW << "LIST OF FLOAT:" << NOCOL << std::endl;
     std::cout << "Creating a list of floats and filling it up with random values between 0.0 and 1.0: " << std::endl;
-    std::list<float> myListF;
+    std::vector<float> myListF;
     srand(time(0));
     for (int i = 0; i < 10; ++i)
         myListF.push_back(static_cast<float>(rand()) / (static_cast<float>(RAND_MAX)));
 
     std::cout << "Printing list using iterators" << std::endl;
-    std::list<float>::iterator itf = myListF.begin();
-    std::list<float>::iterator itef = myListF.end();
+    std::vector<float>::iterator itf = myListF.begin();
+    std::vector<float>::iterator itef = myListF.end();
     for (; itf != itef; ++itf)
         std::cout << std::right << std::setprecision(2) << *itf << "\t";
     std::cout << std::endl;
@@ -77,14 +77,14 @@ int main(void)
 
     std::cout << std::endl;
     std::cout << YELLOW << "LIST OF CHAR:" << NOCOL << std::endl;
-    std::list<char> myListC;
+    std::vector<char> myListC;
     srand(time(0));
     for (int i = 0; i < 10; ++i)
         myListC.push_back('a' + rand() % 26);
 
     std::cout << "Printing list using iterators" << std::endl;
-    std::list<char>::iterator itc = myListC.begin();
-    std::list<char>::iterator itec = myListC.end();
+    std::vector<char>::iterator itc = myListC.begin();
+    std::vector<char>::iterator itec = myListC.end();
     for (; itc != itec; ++itc)
         std::cout << std::right << *itc << "\t";
     std::cout << std::endl;
