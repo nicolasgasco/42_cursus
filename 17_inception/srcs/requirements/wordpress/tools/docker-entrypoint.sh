cbee4444f19faf32c79c6d818dd6e9d1adcaef1b
Nicolas Gasco <nicolas.gasco92@gmail.com>
2022-11-10 21:54:35 +0100

    Inception WIP

--- 17_inception/srcs/requirements/wordpress/tools/docker-entrypoint.sh
+++ 17_inception/srcs/requirements/wordpress/tools/docker-entrypoint.sh
@@ -6,7 +6,9 @@ if [ ! -f "/var/www/html/wp-config.php" ]; then
     wp core download --allow-root
     wp config create --dbname="wp" --dbuser="wp" --dbpass="secret" --dbhost="mariadb:3306" --dbcharset="utf8" --dbcollate="utf8_general_ci" --allow-root
     wp core install --url=$DOMAIN_NAME/wordpress --title="db" --admin_user="wp" --admin_password="secret" --admin_email="test@test.com" --skip-email --allow-root
-    # wp user create "user" "user@test.com" --role=author --user_pass="secret" --allow-root
+    wp user create $WP_USR $WP_EMAIL --role=author --user_pass=$WP_PWD --allow-root
+    
+    wp plugin update --all --allow-root
 fi
-echo "Wordpress started on :9000"
+echo "[i] Wordpress started"
 /usr/sbin/php-fpm7 -F -R
