47f8083db1796408761b0dd23524483d8d5913a7
Nicolas Gasco <nicolas.gasco92@gmail.com>
2022-11-11 22:34:26 +0100

    Stuck at SSL issue

--- 17_inception/srcs/requirements/mariadb/tools/configure.sh
+++ 17_inception/srcs/requirements/mariadb/tools/configure.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 # From https://github.com/yobasystems/alpine-mariadb/blob/master/alpine-mariadb-armhf/files/run.sh
-rm -rf /run/mysqld && rm -rf /var/lib/mysql/mysql
 
 if [ -d "/run/mysqld" ]; then
     echo "[i] mysqld already present, skipping creation"
@@ -16,63 +15,48 @@ if [ -d /var/lib/mysql/mysql ]; then
     echo "[i] MySQL directory already present, skipping creation"
     chown -R mysql:mysql /var/lib/mysql
     
-    echo "[i] MySQL root Password: $MYSQL_ROOT_PASSWORD"
-    echo "[i] MySQL database name is: $MYSQL_DATABASE"
-    echo "[i] MySQL user is: $MYSQL_USER"
-    echo "[i] MySQL password is: $MYSQL_PASSWORD"
+    echo "[i] MySQL root Password: $MYSQL_ROOT_PWD"
+    echo "[i] MySQL database name is: $WP_DATABASE_NAME"
+    echo "[i] MySQL user is: $WP_DATABASE_USR"
+    echo "[i] MySQL password is: $WP_DATABASE_PWD"
 else
     echo "[i] MySQL data directory not found, creating initial DBs"
     chown -R mysql:mysql /var/lib/mysql
     
     mysql_install_db --user=mysql --ldata=/var/lib/mysql > /dev/null
     
-    MYSQL_DATABASE=${MYSQL_DATABASE:-""}
-    MYSQL_USER=${MYSQL_USER:-""}
-    MYSQL_PASSWORD=${MYSQL_PASSWORD:-""}
-    
     tfile=`mktemp`
     
     cat << EOF > $tfile
 USE mysql;
 FLUSH PRIVILEGES ;
-GRANT ALL ON *.* TO 'root'@'%' identified by '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION ;
-GRANT ALL ON *.* TO 'root'@'localhost' identified by '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION ;
-SET PASSWORD FOR 'root'@'localhost'=PASSWORD('${MYSQL_ROOT_PASSWORD}') ;
+GRANT ALL ON *.* TO 'root'@'%' identified by '$MYSQL_ROOT_PWD' WITH GRANT OPTION ;
+GRANT ALL ON *.* TO 'root'@'localhost' identified by '$MYSQL_ROOT_PWD' WITH GRANT OPTION ;
+SET PASSWORD FOR 'root'@'localhost'=PASSWORD('${MYSQL_ROOT_PWD}') ;
 DROP DATABASE IF EXISTS test ;
 FLUSH PRIVILEGES ;
 EOF
     
-    if [ "$MYSQL_DATABASE" != "" ]; then
-        echo "[i] Creating database: $MYSQL_DATABASE"
+    if [ "$WP_DATABASE_NAME" != "" ]; then
+        echo "[i] Creating database: $WP_DATABASE_NAME"
         
         echo "[i] with character set: 'utf8' and collation: 'utf8_general_ci'"
-        echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` CHARACTER SET utf8 COLLATE utf8_general_ci;" >> $tfile
+        echo "CREATE DATABASE IF NOT EXISTS \`$WP_DATABASE_NAME\` CHARACTER SET utf8 COLLATE utf8_general_ci;" >> $tfile
         
-        echo "[i] Creating user: $MYSQL_USER with password $MYSQL_PASSWORD"
-        echo "GRANT ALL ON \`$MYSQL_DATABASE\`.* to '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" >> $tfile
+        echo "[i] Creating user: $WP_DATABASE_USR with password $WP_DATABASE_PWD"
+        echo "GRANT ALL ON \`$WP_DATABASE_NAME\`.* to '$WP_DATABASE_USR'@'%' IDENTIFIED BY '$WP_DATABASE_PWD';" >> $tfile
     fi
     
     /usr/bin/mysqld --user=mysql --bootstrap --verbose=0 --skip-name-resolve --skip-networking=0 < $tfile
     rm -f $tfile
     
-    for f in /docker-entrypoint-initdb.d/*; do
-        case "$f" in
-            *.sql)    echo "$0: running $f"; /usr/bin/mysqld --user=mysql --bootstrap --verbose=0 --skip-name-resolve --skip-networking=0 < "$f"; echo ;;
-            *.sql.gz) echo "$0: running $f"; gunzip -c "$f" | /usr/bin/mysqld --user=mysql --bootstrap --verbose=0 --skip-name-resolve --skip-networking=0 < "$f"; echo ;;
-            *)        echo "$0: ignoring or entrypoint initdb empty $f" ;;
-        esac
-        echo
-    done
-    
-    echo
+    /usr/bin/mysqld --user=mysql --bootstrap < $tfile
+    rm -f $tfile
     echo 'MySQL init process done. Ready for start up.'
-    echo
-    
-    echo "exec /usr/bin/mysqld --user=mysql --console --skip-name-resolve --skip-networking=0" "$@"
 fi
 
 # allow remote connections
 sed -i "s|skip-networking|# skip-networking|g" /etc/my.cnf.d/mariadb-server.cnf
 sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/my.cnf.d/mariadb-server.cnf
 
-exec /usr/bin/mysqld --user=mysql --console --skip-name-resolve --skip-networking=0 $@
+exec /usr/bin/mysqld --user=mysql --console
--- 17_inception/srcs/requirements/wordpress/tools/docker-entrypoint.sh
+++ 17_inception/srcs/requirements/wordpress/tools/docker-entrypoint.sh
@@ -4,10 +4,9 @@ if [ ! -f "/var/www/html/wp-config.php" ]; then
     
     echo "[i] Installing Wordpress"
     wp core download --allow-root
-    wp config create --dbname="wp" --dbuser="wp" --dbpass="secret" --dbhost="mariadb:3306" --dbcharset="utf8" --dbcollate="utf8_general_ci" --allow-root
-    wp core install --url=$DOMAIN_NAME/wordpress --title="db" --admin_user="wp" --admin_password="secret" --admin_email="test@test.com" --skip-email --allow-root
+    wp config create --dbname=$WP_DATABASE_NAME --dbuser=$WP_DATABASE_USR --dbpass=$WP_DATABASE_PWD --dbhost=$MYSQL_HOST --dbcharset="utf8" --dbcollate="utf8_general_ci" --allow-root
+    wp core install --url=$DOMAIN_NAME/wordpress --title=$WP_TITLE --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
     wp user create $WP_USR $WP_EMAIL --role=author --user_pass=$WP_PWD --allow-root
-    
     wp plugin update --all --allow-root
 fi
 echo "[i] Wordpress started"
