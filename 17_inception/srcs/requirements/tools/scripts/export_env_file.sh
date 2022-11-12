fff50c5a61b92569a3d38c14b47b2ae353e60341
Nicolas Gasco <nicolas.gasco92@gmail.com>
2022-11-12 17:16:13 +0100

    Wordpress finally working

---
+++ 17_inception/srcs/requirements/tools/scripts/export_env_file.sh
@@ -0,0 +1,1 @@
+export $(grep -v '^#' .env | xargs)
--- 17_inception/srcs/requirements/wordpress/tools/docker-entrypoint.sh
+++ 17_inception/srcs/requirements/wordpress/tools/docker-entrypoint.sh
@@ -2,7 +2,7 @@
 
 if [ ! -f "/var/www/html/wp-config.php" ]; then
     
-    echo "[i] Installing Wordpress"
+    echo "[i] Installing Wordpress with CLI"
     wp core download --allow-root
     wp config create --dbname=$WP_DATABASE_NAME --dbuser=$WP_DATABASE_USR --dbpass=$WP_DATABASE_PWD --dbhost=$MYSQL_HOST --dbcharset="utf8" --dbcollate="utf8_general_ci" --allow-root
     wp core install --url=$DOMAIN_NAME/wordpress --title=$WP_TITLE --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
@@ -11,3 +11,6 @@ if [ ! -f "/var/www/html/wp-config.php" ]; then
 fi
 echo "[i] Wordpress started"
 /usr/sbin/php-fpm7 -F -R
+
+# Move custom index file
+mv ../../../../tmp/index.html ../
