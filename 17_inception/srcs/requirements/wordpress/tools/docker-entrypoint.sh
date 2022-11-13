5a204dc180630763efa84ce29866b3a344924883
Nicolas Gasco <nicolas.gasco92@gmail.com>
2022-11-13 22:16:48 +0100

    Fix domain and trim unused code

--- 17_inception/srcs/requirements/wordpress/tools/docker-entrypoint.sh
+++ 17_inception/srcs/requirements/wordpress/tools/docker-entrypoint.sh
@@ -9,6 +9,7 @@ echo "    tWP_DATABASE_PWD: $WP_DATABASE_PWD"
 
 if [ ! -f "/var/www/html/wordpress/wp-config.php" ]; then
     echo "[i] Installing Wordpress with CLI"
+    wp cli update
     wp core download --allow-root
     wp config create --dbname=$WP_DATABASE_NAME --dbuser=$WP_DATABASE_USR --dbpass=$WP_DATABASE_PWD --dbhost=$MYSQL_HOST --dbcharset="utf8" --dbcollate="utf8_general_ci" --allow-root
     wp core install --url=$DOMAIN_NAME/wordpress --title=$WP_TITLE --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
