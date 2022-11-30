041996c535173f1f8e6bd28f312e94129576db77
Nicolas Gasco <nicolas.gasco92@gmail.com>
2022-11-30 23:00:40 +0100

    Add push_back cases to back() unit tests

--- 18_ft_containers/unit_tests/vector/access/back.hpp
+++ 18_ft_containers/unit_tests/vector/access/back.hpp
@@ -46,4 +46,37 @@ void backUnitTests(bool isDebug)
         std::vector<int> const original(5, 10);
         outputAssertion("is const with fill costructor (num: 5, value:10):", isStrictEqual<bool>(isConst(own.back()), isConst(original.back()), isDebug));
     }
+
+    std::cout << std::endl
+              << "Value is equal to STL vector value:" << std::endl;
+    {
+        ft::vector<int> own;
+        own.reserve(1);
+        own.push_back(5);
+        std::vector<int> original;
+        original.reserve(1);
+        original.push_back(5);
+        outputAssertion("after push_back without reallocation:", isStrictEqual<int>(own.back(), original.back(), isDebug));
+    }
+    {
+        ft::vector<int> own;
+        own.push_back(5);
+        std::vector<int> original;
+        original.push_back(5);
+        outputAssertion("after push_back with allocation (size = 0):", isStrictEqual<int>(own.back(), original.back(), isDebug));
+    }
+    {
+        ft::vector<int> own(1, 10);
+        own.push_back(5);
+        std::vector<int> original(1, 10);
+        original.push_back(5);
+        outputAssertion("after push_back with allocation (size = 1):", isStrictEqual<int>(own.back(), original.back(), isDebug));
+    }
+    {
+        ft::vector<int> own(10, 10);
+        own.push_back(5);
+        std::vector<int> original(10, 10);
+        original.push_back(5);
+        outputAssertion("after push_back with allocation (size = 10):", isStrictEqual<int>(own.back(), original.back(), isDebug));
+    }
 }
