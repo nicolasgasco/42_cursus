c94db743a946d2694756be85b1ad4a23968fa1c2
Nicolas Gasco <nicolas.gasco92@gmail.com>
2022-12-02 20:17:34 +0100

    Order member types

--- 18_ft_containers/vector/Vector.hpp
+++ 18_ft_containers/vector/Vector.hpp
@@ -13,14 +13,14 @@ namespace ft
          */
         typedef T value_type;
         typedef Allocator allocator_type;
+        typedef typename allocator_type::size_type size_type;
+        typedef typename allocator_type::difference_type difference_type;
         typedef typename allocator_type::reference reference;
         typedef typename allocator_type::const_reference const_reference;
-        // typedef implementation - defined iterator;
-        // typedef implementation - defined const_iterator;
-        typedef typename allocator_type::size_type size_type;
-        // typedef typename allocator_type::difference_type difference_type;
         typedef typename allocator_type::pointer pointer;
-        // typedef typename allocator_type::const_pointer const_pointer;
+        typedef typename allocator_type::const_pointer const_pointer;
+        // typedef allocator_type::pointer iterator;
+        // typedef implementation - defined const_iterator;
         // typedef std::reverse_iterator<iterator> reverse_iterator;
         // typedef std::reverse_iterator<const_iterator> const_reverse_iterator;
 
