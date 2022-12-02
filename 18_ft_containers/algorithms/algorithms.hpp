46a96a1c4244331b33e50590c52db4e6584252e6
Nicolas Gasco <nicolas.gasco92@gmail.com>
2022-12-02 20:12:25 +0100

    Add algorithms file

---
+++ 18_ft_containers/algorithms/algorithms.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <iostream>
+
+namespace ft
+{
+    template <class InputIterator1, class InputIterator2>
+    bool equal(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2)
+    {
+        if (first1 && last1 && first2)
+        {
+        }
+    }
+
+    template <class InputIterator1, class InputIterator2, class BinaryPredicate>
+    bool equal(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, BinaryPredicate pred)
+    {
+        if (first1 && last1 && first2 && pred)
+        {
+        }
+    }
+
+    template <class InputIterator1, class InputIterator2>
+    bool lexicographical_compare(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, InputIterator2 last2)
+    {
+        if (first1 && last1 && first2 && last2)
+        {
+        }
+    }
+
+    template <class InputIterator1, class InputIterator2, class Compare>
+    bool lexicographical_compare(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, InputIterator2 last2, Compare comp)
+    {
+        if (first1 && last1 && first2 && last2 && comp)
+        {
+        }
+    }
+}
