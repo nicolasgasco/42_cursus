9e65039dbaf614e3a940c02b03546ebabcddb016
Nicolas Gasco <nicolas.gasco92@gmail.com>
2022-12-02 20:40:40 +0100

    Add ft::iterator_traits and use it in vector

---
+++ 18_ft_containers/iterators/iterator_traits.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <iostream>
+
+namespace ft
+{
+    template <class Iter>
+    struct iterator_traits
+    {
+        typedef typename Iter::value_type value_type;
+        typedef typename Iter::difference_type difference_type;
+        typedef typename Iter::pointer pointer;
+        typedef typename Iter::reference reference;
+        typedef typename Iter::iterator_category iterator_category;
+    };
+
+    template <class T>
+    struct iterator_traits<T *>
+    {
+        typedef T value_type;
+        typedef std::ptrdiff_t difference_type;
+        typedef T *pointer;
+        typedef T &reference;
+        typedef std::random_access_iterator_tag iterator_category;
+    };
+
+    template <class T>
+    struct iterator_traits<const T *>
+    {
+        typedef T value_type;
+        typedef std::ptrdiff_t difference_type;
+        typedef const T *pointer;
+        typedef const T &reference;
+        typedef std::random_access_iterator_tag iterator_category;
+    };
+}
--- 18_ft_containers/vector/Vector.hpp
+++ 18_ft_containers/vector/Vector.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "../iterators/iterator_traits.hpp"
+
 #include <iostream>
 
 namespace ft
@@ -19,8 +21,8 @@ namespace ft
         typedef typename allocator_type::const_reference const_reference;
         typedef typename allocator_type::pointer pointer;
         typedef typename allocator_type::const_pointer const_pointer;
-        // typedef allocator_type::pointer iterator;
-        // typedef implementation - defined const_iterator;
+        typedef typename ft::iterator_traits<pointer>::iterator_category iterator;
+        typedef typename ft::iterator_traits<const_pointer>::iterator_category const_iterator;
         // typedef std::reverse_iterator<iterator> reverse_iterator;
         // typedef std::reverse_iterator<const_iterator> const_reverse_iterator;
 
