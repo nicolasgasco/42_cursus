5d3fe5f3b303ed09306adf476f8f4dcc374bc146
Nicolas Gasco <nicolas.gasco92@gmail.com>
2022-12-09 18:47:57 +0100

    Add size method tests

---
+++ 18_ft_containers/unit_tests/stack/capacity/size.cpp
@@ -0,0 +1,59 @@
+#include <stack>
+#include <deque>
+#include <list>
+
+#include "utils.hpp"
+#include "../stack.hpp"
+
+void sizeStackUnitTests(bool isDebug)
+{
+    outputSuiteTitle("SIZE:");
+    {
+        std::cout
+            << "Return value is equal:" << std::endl;
+        {
+            {
+                ft::stack<int> ownStack;
+
+                std::stack<int> originalStack;
+
+                outputAssertion("with stack<int> of size 0", isStrictEqual<int>(ownStack.size(), originalStack.size(), isDebug));
+            }
+            {
+                ft::stack<int> ownStack;
+                ownStack.push(42);
+
+                std::stack<int> originalStack;
+                originalStack.push(42);
+
+                outputAssertion("with stack<int> of size 1", isStrictEqual<int>(ownStack.size(), originalStack.size(), isDebug));
+            }
+            {
+                ft::stack<int> ownStack;
+                ownStack.push(42);
+                ownStack.push(42);
+
+                std::stack<int> originalStack;
+                originalStack.push(42);
+                originalStack.push(42);
+
+                outputAssertion("with stack<int> of size 2", isStrictEqual<int>(ownStack.size(), originalStack.size(), isDebug));
+            }
+            {
+                ft::stack<int> ownStack;
+                ownStack.push(42);
+                ownStack.push(42);
+                ownStack.pop();
+                ownStack.pop();
+
+                std::stack<int> originalStack;
+                originalStack.push(42);
+                originalStack.push(42);
+                originalStack.pop();
+                originalStack.pop();
+
+                outputAssertion("with stack<int> of size 2 + 2 pop()", isStrictEqual<int>(ownStack.size(), originalStack.size(), isDebug));
+            }
+        }
+    }
+}
