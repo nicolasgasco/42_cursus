d7aab7eb750b8112ff8654d8a56fe53405e65dab
Nicolas Gasco <nicolas.gasco92@gmail.com>
2022-12-09 18:22:49 +0100

    Add assign operator tests

---
+++ 18_ft_containers/unit_tests/stack/lifecycle/assign.cpp
@@ -0,0 +1,85 @@
+#include <stack>
+#include <deque>
+#include <list>
+
+#include "utils.hpp"
+#include "../stack.hpp"
+
+void assignStackUnitTests(bool isDebug)
+{
+    outputSuiteTitle("ASSIGN:");
+    {
+        std::cout
+            << "Stacks are equal:" << std::endl;
+        {
+            std::cout << "  - NOT CONST" << std::endl;
+            {
+                ft::stack<int> ownRef;
+                ownRef.push(42);
+                ownRef.push(1);
+                ownRef.push(100);
+                ft::stack<int> ownStack;
+                ownStack = ownRef;
+
+                std::stack<int> originalRef;
+                originalRef.push(42);
+                originalRef.push(1);
+                originalRef.push(100);
+                std::stack<int> originalStack;
+                originalStack = originalRef;
+
+                outputAssertion("when assigning to another stack<int>", areStacksStrictEqual<int>(ownStack, originalStack, isDebug));
+            }
+            {
+                ft::stack<std::string> ownRef;
+                ownRef.push("foo");
+                ownRef.push("bar");
+                ft::stack<std::string> ownStack;
+                ownStack = ownRef;
+
+                std::stack<std::string> originalRef;
+                originalRef.push("foo");
+                originalRef.push("bar");
+                std::stack<std::string> originalStack;
+                originalStack = originalRef;
+
+                outputAssertion("when assigning to another stack<std::string>", areStacksStrictEqual<std::string>(ownStack, originalStack, isDebug));
+            }
+        }
+        {
+            std::cout << "  - CONST" << std::endl;
+            {
+                ft::stack<int> ownRef;
+                ownRef.push(42);
+                ownRef.push(1);
+                ownRef.push(100);
+                ft::stack<int> ownStack;
+                ownStack = const_cast<ft::stack<int> &>(ownRef);
+
+                std::stack<int> originalRef;
+                originalRef.push(42);
+                originalRef.push(1);
+                originalRef.push(100);
+                std::stack<int> originalStack;
+                originalStack = const_cast<std::stack<int> &>(originalRef);
+
+                outputAssertion("when assigning to another stack<int>", areStacksStrictEqual<int>(ownStack, originalStack, isDebug));
+            }
+            {
+                ft::stack<std::string> ownRef;
+                ownRef.push("foo");
+                ownRef.push("bar");
+                ft::stack<std::string> ownStack;
+                ownStack = static_cast<ft::stack<std::string> &>(ownRef);
+
+                std::stack<std::string> originalRef;
+                originalRef.push("foo");
+                originalRef.push("bar");
+                std::stack<std::string> originalStack;
+                originalStack = const_cast<std::stack<std::string> &>(originalRef);
+
+                outputAssertion("when assigning to another stack<std::string>", areStacksStrictEqual<std::string>(ownStack, originalStack, isDebug));
+            }
+        }
+    }
+}
--- 18_ft_containers/unit_tests/stack/stack.cpp
+++ 18_ft_containers/unit_tests/stack/stack.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 
 #include "lifecycle/constructor.cpp"
+#include "lifecycle/assign.cpp"
 
 void stackUnitTests(bool IS_DEBUG)
 {
     std::cout << std::endl
               << YELLOW << "STACK UNIT TESTS" << NOCOL << std::endl;
     constructorUnitTests(IS_DEBUG);
+    assignStackUnitTests(IS_DEBUG);
 }
