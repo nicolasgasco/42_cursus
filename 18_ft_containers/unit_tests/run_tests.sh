bcab95d914c1b10ad8f1e9e5d21b27664710f3f0
Nicolas Gasco <nicolas.gasco92@gmail.com>
2022-12-11 00:59:40 +0100

    Move all unit tests to shell script

--- 18_ft_containers/unit_tests/run_tests.sh
+++ 18_ft_containers/unit_tests/run_tests.sh
@@ -8,8 +8,8 @@ do
         d) DEBUG="debug";;
         v) $CC $FLAGS vector/vector_tests.cpp -o vector/vector_tests  && ./vector/vector_tests $DEBUG;;
         s) $CC $FLAGS stack/stack.cpp -o stack/stack_tests && ./stack/stack_tests $DEBUG;;
-        a) $CC $FLAGS algorithms/algorithms_tests.cpp -o algorithms_tests && ./algorithms/algorithms_tests $DEBUG;;
-        r) $CC $FLAGS reverse_iterator/reverse_iterator.cpp -o reverse_iterator_tests && ./reverse_iterator/reverse_iterator_tests $DEBUG;;
-        p) $CC $FLAGS pair/pair_tests.cpp -o pair_tests && ./pair/pair_tests $DEBUG;;
+        a) $CC $FLAGS algorithms/algorithms_tests.cpp -o algorithms/algorithms_tests && ./algorithms/algorithms_tests $DEBUG;;
+        r) $CC $FLAGS reverse_iterator/reverse_iterator.cpp -o reverse_iterator/reverse_iterator_tests && ./reverse_iterator/reverse_iterator_tests $DEBUG;;
+        p) $CC $FLAGS pair/pair_tests.cpp -o pair/pair_tests && ./pair/pair_tests $DEBUG;;
     esac
 done
