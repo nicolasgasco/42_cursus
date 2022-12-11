af033a29628e7385916b25347d541b75b4c253a7
Nicolas Gasco <nicolas.gasco92@gmail.com>
2022-12-11 20:11:05 +0100

    value_compare subclass WIP

--- 18_ft_containers/map.hpp
+++ 18_ft_containers/map.hpp
@@ -12,28 +12,47 @@ namespace ft
     template <class Key, class T, class Compare = std::less<Key>, class Allocator = std::allocator<typename ft::pair<const Key, T> > >
     class map
     {
+
     public:
-        /*
-         * MEMBER TYPES
-         */
         typedef Key key_type;
         typedef T mapped_type;
         typedef typename ft::pair<const key_type, mapped_type> value_type;
-        typedef Compare key_compare;
-        // typedef   value_compare;
         typedef Allocator allocator_type;
+        typedef typename allocator_type::size_type size_type;
+        typedef Compare key_compare;
         typedef typename allocator_type::reference reference;
         typedef typename allocator_type::const_reference const_reference;
         typedef typename allocator_type::pointer pointer;
         typedef typename allocator_type::const_pointer const_pointer;
         typedef typename ft::iterator<typename ft::pair<Key, T> > iterator;
         typedef typename ft::iterator<typename ft::pair<const Key, T> > const_iterator;
+        typedef typename ft::iterator_traits<iterator>::difference_type difference_type;
         typedef typename ft::reverse_iterator<iterator> reverse_iterator;
         typedef typename ft::reverse_iterator<const_iterator> const_reverse_iterator;
-        typedef typename ft::iterator_traits<iterator>::difference_type difference_type;
-        typedef typename allocator_type::size_type size_type;
 
     protected:
+        /*
+         * VALUE COMPARE SUBCLASS
+         */
+        class value_compare : public std::binary_function<value_type, value_type, bool>
+        {
+        public:
+            typedef bool result_type;
+            typedef value_type first_argument_type;
+            typedef value_type second_argument_type;
+
+            bool operator()(const value_type &lhs, const value_type &rhs) const
+            {
+                return comp(lhs.first, rhs.first);
+            }
+
+        protected:
+            key_compare _comp;
+
+            value_compare(key_compare c) : _comp(c)
+            {}
+        };
+
         allocator_type _alloc;
         key_compare _comp;
 
@@ -59,7 +78,7 @@ namespace ft
 
         map &operator=(const map &x);
 
-        ~map()
+        virtual ~map()
         {
         }
     };
