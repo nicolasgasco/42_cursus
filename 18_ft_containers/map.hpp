a705de22668b253cd2e2dfbee8bdc08de1e03e0e
Nicolas Gasco <nicolas.gasco92@gmail.com>
2022-12-11 19:39:53 +0100

    Init map with member types

---
+++ 18_ft_containers/map.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "algorithms.hpp"
+#include "enable_if.hpp"
+#include "iterator_traits.hpp"
+#include "reverse_iterator.hpp"
+#include "pair.hpp"
+#include <iostream>
+
+namespace ft
+{
+    template <class Key, class T, class Compare = std::less<Key>, class Allocator = std::allocator<typename ft::pair<const Key, T> > >
+    class map
+    {
+    public:
+        /*
+         * MEMBER TYPES
+         */
+        typedef Key key_type;
+        typedef T mapped_type;
+        typedef typename ft::pair<const key_type, mapped_type> value_type;
+        typedef Compare key_compare;
+        // typedef   value_compare;
+        typedef Allocator allocator_type;
+        typedef typename allocator_type::reference reference;
+        typedef typename allocator_type::const_reference const_reference;
+        typedef typename allocator_type::pointer pointer;
+        typedef typename allocator_type::const_pointer const_pointer;
+        typedef typename ft::iterator<typename ft::pair<Key, T> > iterator;
+        typedef typename ft::iterator<typename ft::pair<const Key, T> > const_iterator;
+        typedef typename ft::reverse_iterator<iterator> reverse_iterator;
+        typedef typename ft::reverse_iterator<const_iterator> const_reverse_iterator;
+        typedef typename ft::iterator_traits<iterator>::difference_type difference_type;
+        typedef typename allocator_type::size_type size_type;
+    };
+}
