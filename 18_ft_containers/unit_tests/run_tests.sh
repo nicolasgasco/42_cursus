2ca0bd5acc0a99bdc1cac6995faf6ba3c1c5cfa0
Nicolas Gasco <nicolas.gasco92@gmail.com>
2023-01-16 23:20:08 +0100

    Remove extra space

--- 18_ft_containers/unit_tests/run_tests.sh
+++ 18_ft_containers/unit_tests/run_tests.sh
@@ -67,7 +67,7 @@ do
     case "${flag}" in
         d) DEBUG="debug";;
         v) $CC $FLAGS vector/vector_tests.cpp -o vector/vector_tests  && ./vector/vector_tests $DEBUG > $REPORT_FILE;;
-        m) $CC $FLAGS map/map_tests.cpp -o map/map_tests  && ./map/map_tests $DEBUG > $REPORT_FILE;;
+        m) $CC $FLAGS map/map_tests.cpp -o map/map_tests && ./map/map_tests $DEBUG > $REPORT_FILE;;
         s) $CC $FLAGS stack/stack.cpp -o stack/stack_tests && ./stack/stack_tests $DEBUG > $REPORT_FILE;;
         a) $CC $FLAGS algorithms/algorithms_tests.cpp -o algorithms/algorithms_tests && ./algorithms/algorithms_tests $DEBUG  > $REPORT_FILE;;
         r) $CC $FLAGS reverse_iterator/reverse_iterator.cpp -o reverse_iterator/reverse_iterator_tests && ./reverse_iterator/reverse_iterator_tests $DEBUG  > $REPORT_FILE;;
