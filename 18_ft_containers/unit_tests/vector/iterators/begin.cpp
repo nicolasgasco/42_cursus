9828123de9bd1e56e35c3de70ce7805d5bb595f4
Nicolas Gasco <nicolas.gasco92@gmail.com>
2023-01-20 23:47:57 +0100

    Add tests for begin() method of vector

--- 18_ft_containers/unit_tests/vector/iterators/begin.cpp
+++ 18_ft_containers/unit_tests/vector/iterators/begin.cpp
@@ -56,5 +56,31 @@ void beginUnitTests(bool isDebug)
             std::vector<int>::iterator originalIt = original.begin();
             outputAssertion("with default constructor + push_back(10) + push_back(5):", isStrictEqual<int>(*ownIt, *originalIt, isDebug));
         }
+        {
+            ft::vector<int> own;
+            own.push_back(10);
+            own.push_back(5);
+            ft::vector<int>::iterator ownIt = own.begin();
+
+            std::vector<int> original;
+            original.push_back(10);
+            original.push_back(5);
+            std::vector<int>::iterator originalIt = original.begin();
+
+            outputAssertion("after post-incrementing vector with 2 elements:", isStrictEqual<int>(*(ownIt++), *(originalIt++), isDebug));
+        }
+        {
+            ft::vector<int> own;
+            own.push_back(10);
+            own.push_back(5);
+            ft::vector<int>::iterator ownIt = own.begin();
+
+            std::vector<int> original;
+            original.push_back(10);
+            original.push_back(5);
+            std::vector<int>::iterator originalIt = original.begin();
+
+            outputAssertion("after pre-incrementing vector with 2 elements:", isStrictEqual<int>(*(++ownIt), *(++originalIt), isDebug));
+        }
     }
 }
