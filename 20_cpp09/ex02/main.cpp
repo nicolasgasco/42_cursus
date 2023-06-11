9fa840c735c446268ef6b585f9737b033a567ecc
Nicolas Gasco <nicolas.gasco92@gmail.com>
2023-06-11 20:05:16 +0200

    Validate arguments received as input

--- 20_cpp09/ex02/main.cpp
+++ 20_cpp09/ex02/main.cpp
@@ -16,6 +16,23 @@ void output_args(int argc, char *argv[])
     std::cout << std::endl;
 }
 
+bool is_input_valid(int argc, char *argv[])
+{
+    for (int i = 1; i < argc; i++)
+    {
+        for (unsigned int j = 0; j < strlen(argv[i]); j++)
+        {
+            if (!std::isdigit(argv[i][j]))
+                return false;
+        }
+
+        int value = std::atoi(argv[i]);
+        if (value < 0)
+            return false;
+    }
+    return true;
+}
+
 int main(int argc, char *argv[])
 {
     if (argc < 2)
@@ -23,9 +40,22 @@ int main(int argc, char *argv[])
         std::cerr << "Please provide an argument" << std::endl;
         return 1;
     }
+    else if (argc == 2)
+    {
+        std::cerr << "Provide more than one number" << std::endl;
+        return 1;
+    }
+
+    if (!is_input_valid(argc, argv))
+    {
+        std::cerr << "Invalid input" << std::endl;
+        return 1;
+    }
 
     std::cout << "Before: ";
     output_args(argc, argv);
+
+    PmergeMe pmergeMe;
 
     return 0;
 }
