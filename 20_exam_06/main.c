643807b186b78beb71059fa761e252d6d964cf17
Nicolas Gasco <nicolas.gasco92@gmail.com>
2023-06-15 20:12:58 +0200

    Expand logic to receive unlimited messages

--- 20_exam_06/main.c
+++ 20_exam_06/main.c
@@ -7,7 +7,6 @@
 #include <stdlib.h>
 #include <printf.h>
 
-#define PORT 8081
 #define BUFF_SIZE 30000
 
 int extract_message(char **buf, char **msg)
@@ -37,8 +36,16 @@ int extract_message(char **buf, char **msg)
     return (0);
 }
 
-int main(void)
+int main(int argc, char **argv)
 {
+    if (argc != 2)
+    {
+        printf("Wrong number of arguments\n");
+        return 1;
+    }
+
+    int port = atoi(argv[1]);
+
     int sockfd, connfd, len;
     struct sockaddr_in servaddr, cli;
 
@@ -46,65 +53,87 @@ int main(void)
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd == -1)
     {
-        printf("socket creation failed...\n");
-        exit(0);
+        printf("Fatal error\n");
+        exit(1);
     }
     else
-        printf("Socket successfully created..\n");
+        printf("Socket successfully created..\n"); // Remove before submission
     bzero(&servaddr, sizeof(servaddr));
 
     // assign IP, PORT
     servaddr.sin_family = AF_INET;
     servaddr.sin_addr.s_addr = htonl(2130706433); // 127.0.0.1
-    servaddr.sin_port = htons(PORT);
+    servaddr.sin_port = htons(port);
+
+    // Binding newly created socket to given IP and verification
+    if ((bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr))) != 0)
+    {
+        printf("Fatal error\n");
+        exit(1);
+    }
+    else
+        printf("Socket successfully binded..\n"); // Remove before submission
+
+    if (listen(sockfd, 10) != 0)
+    {
+        printf("Fatal error\n");
+        exit(1);
+    }
+    else
+        printf("Server listening..\n"); // Remove before submission
+
+    len = sizeof(cli);
+
+    // ACCEPT CONNECTION
+    connfd = accept(sockfd, (struct sockaddr *)&cli, (socklen_t *)&len);
+    if (connfd < 0)
+    {
+        printf("server acccept failed...\n");
+        exit(0);
+    }
+    else
+        printf("server acccept the client...\n"); // Remove before submission
 
     while (1)
     {
-
-        // Binding newly created socket to given IP and verification
-        if ((bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr))) != 0)
-        {
-            printf("socket bind failed...\n");
-            exit(0);
-        }
-        else
-            printf("Socket successfully binded..\n");
-        if (listen(sockfd, 10) != 0)
-        {
-            printf("cannot listen\n");
-            exit(0);
-        }
-        len = sizeof(cli);
-        connfd = accept(sockfd, (struct sockaddr *)&cli, (socklen_t *)&len);
-        if (connfd < 0)
-        {
-            printf("server acccept failed...\n");
-            exit(0);
-        }
-        else
-            printf("server acccept the client...\n");
-
-        printf("Ready to receive\n");
-
         char *msg = (char *)malloc(sizeof(char) * BUFF_SIZE);
         char *buf = (char *)malloc(sizeof(char) * BUFF_SIZE);
+        if (msg == NULL || buf == NULL)
+        {
+            printf("Fatal error\n");
+            return 1;
+        }
 
         int bytes_recv = recv(connfd, buf, BUFF_SIZE, 0);
+
+        switch (bytes_recv)
+        {
+        case 0:
+            continue;
+        }
+
         printf("Received %d bytes\n", bytes_recv);
 
         int extract_res = -1;
-        while (extract_res != 0)
+        while (1)
         {
             extract_res = extract_message(&buf, &msg);
-            printf("Extract result: %d\n", extract_res);
+            if (extract_res == -1)
+            {
+                printf("Error: %s\n", strerror(errno));
+                return 1;
+            }
+
+            if (extract_res == 0)
+                break;
+
             printf("Message: .%s.\n", msg);
         }
-
+        free(msg);
         free(buf);
-
-        printf("Closing connection\n");
-        close(sockfd);
-        close(connfd);
     }
+    printf("Closing connection\n");
+    close(sockfd);
+    close(connfd);
     return 1;
 }
