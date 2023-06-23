6603c969cdbc958e0c99edfd9265411b4c37395c
Nicolas Gasco <nicolas.gasco92@gmail.com>
2023-06-23 14:38:04 +0200

    Second attempt

--- 20_exam_06/mini_serv.c
+++ 20_exam_06/mini_serv.c
@@ -6,239 +6,272 @@
 #include <netinet/in.h>
 #include <stdlib.h>
 #include <printf.h>
-#include <stdio.h>
 
 int extract_message(char **buf, char **msg)
 {
-    char *newbuf;
-    int i;
+	char *newbuf;
+	int i;
 
-    *msg = 0;
-    if (*buf == 0)
-        return (0);
-    i = 0;
-    while ((*buf)[i])
-    {
-        if ((*buf)[i] == '\n')
-        {
-            newbuf = calloc(1, sizeof(*newbuf) * (strlen(*buf + i + 1) + 1));
-            if (newbuf == 0)
-                return (-1);
-            strcpy(newbuf, *buf + i + 1);
-            *msg = *buf;
-            (*msg)[i + 1] = 0;
-            *buf = newbuf;
-            return (1);
-        }
-        i++;
-    }
-    return (0);
+	*msg = 0;
+	if (*buf == 0)
+		return (0);
+	i = 0;
+	while ((*buf)[i])
+	{
+		if ((*buf)[i] == '\n')
+		{
+			newbuf = calloc(1, sizeof(*newbuf) * (strlen(*buf + i + 1) + 1));
+			if (newbuf == 0)
+				return (-1);
+			strcpy(newbuf, *buf + i + 1);
+			*msg = *buf;
+			(*msg)[i + 1] = 0;
+			*buf = newbuf;
+			return (1);
+		}
+		i++;
+	}
+	return (0);
 }
 
-void add_to_array(int *array, int value)
+char *str_join(char *buf, char *add)
 {
-    int i = 0;
-    while (array[i])
-        i++;
-    array[i] = value;
+	char *newbuf;
+	int len;
+
+	if (buf == 0)
+		len = 0;
+	else
+		len = strlen(buf);
+	newbuf = malloc(sizeof(*newbuf) * (len + strlen(add) + 1));
+	if (newbuf == 0)
+		return (0);
+	newbuf[0] = 0;
+	if (buf != 0)
+		strcat(newbuf, buf);
+	free(buf);
+	strcat(newbuf, add);
+	return (newbuf);
 }
 
-void remove_from_array(int *array, int value)
+void ft_putstr(int fd, char *str)
 {
-    int i = 0;
-    while (array[i])
-    {
-        if (array[i] == value)
-            array[i] = -1;
-        i++;
-    }
+	write(fd, str, strlen(str));
 }
 
-int ft_find_value_in_arr(int *array, int value)
+void ft_putint(int fd, char *str, int value)
 {
-    int i = 0;
-    while (array[i])
-    {
-        if (array[i] == value)
-            return i;
-        i++;
-    }
-    return -1;
+	char output[100];
+	bzero(output, sizeof(output));
+
+	sprintf(output, str, value);
+	ft_putstr(fd, output);
 }
 
-void ft_print_int(int fd, char *msg, int value)
+void ft_add_connection(int *active_fds, int connfd)
 {
-    char print_msg[100];
-    sprintf(print_msg, msg, value);
-    write(fd, print_msg, strlen(print_msg));
+	int i = 0;
+	while (active_fds[i])
+		i++;
+	active_fds[i] = connfd;
 }
 
-void ft_print_str(int fd, char *msg)
+int ft_find_connection(int *active_fds, int connfd)
 {
-    write(fd, msg, strlen(msg));
+	int i = 0;
+	while (active_fds[i])
+	{
+		if (active_fds[i] == connfd)
+			return i;
+		i++;
+	}
+	return -1;
 }
 
-void print_to_clients(char *msg, int *active_connections, int connfd)
+void ft_remove_connection(int *active_fds, int connfd)
 {
-    int j = 0;
-    while (active_connections[j])
-    {
-        if (active_connections[j] != connfd && active_connections[j] != -1)
-            ft_print_int(active_connections[j], msg, ft_find_value_in_arr(active_connections, connfd));
-        j++;
-    }
-    ft_print_int(1, msg, ft_find_value_in_arr(active_connections, connfd));
+	int i = 0;
+	while (active_fds[i])
+	{
+		if (active_fds[i] == connfd)
+			active_fds[i] = -1;
+		i++;
+	}
+}
+
+int ft_find_newline(char *buf)
+{
+	int i = 0;
+	while (buf[i])
+	{
+		if (buf[i] == '\n')
+			return i;
+		i++;
+	}
+	return -1;
 }
 
 int main(int argc, char **argv)
 {
-    if (argc != 2)
-    {
-        ft_print_str(2, "Wrong number of arguments\n");
-        return 1;
-    }
+	if (argc != 2)
+	{
+		ft_putstr(2, "Wrong number of arguments\n");
+		exit(1);
+	}
 
-    int port = atoi(argv[1]);
+	int port = atoi(argv[1]);
 
-    int sockfd, connfd, len;
-    struct sockaddr_in servaddr = {0};
-    bzero(&servaddr, sizeof(servaddr));
+	// socket create and verification
+	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd == -1)
+	{
+		ft_putstr(2, "Fatal error\n");
+		exit(1);
+	}
 
-    // socket create and verification
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd == -1)
-    {
-        ft_print_str(2, "Fatal error\n");
-        exit(1);
-    }
+	struct sockaddr_in servaddr;
+	bzero(&servaddr, sizeof(servaddr));
 
-    // assign IP, PORT
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_addr.s_addr = 16777343;
-    servaddr.sin_port = htons(port);
+	// assign IP, PORT
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_addr.s_addr = htonl(2130706433); // 127.0.0.1
+	servaddr.sin_port = htons(port);
 
-    // Binding newly created socket to given IP and verification
-    if ((bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr))) != 0)
-    {
-        ft_print_str(2, "Fatal error\n");
-        exit(1);
-    }
+	// Binding newly created socket to given IP and verification
+	if ((bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr))) != 0)
+	{
+		ft_putstr(2, "Fatal error\n");
+		close(sockfd);
+		exit(1);
+	}
 
-    if (listen(sockfd, 128) != 0)
-    {
-        ft_print_str(2, "Fatal error\n");
-        exit(1);
-    }
+	if (listen(sockfd, 10) != 0)
+	{
+		ft_putstr(2, "Fatal error\n");
+		close(sockfd);
+		exit(1);
+	}
 
-    // SELECT
-    fd_set read_fds, read_fds_cpy;
-    FD_ZERO(&read_fds);
-    FD_ZERO(&read_fds_cpy);
+	fd_set read_fds, read_fds_cpy;
+	FD_ZERO(&read_fds);
+	FD_ZERO(&read_fds_cpy);
 
-    FD_SET(sockfd, &read_fds_cpy);
+	FD_SET(sockfd, &read_fds_cpy);
 
-    int active_connections[FD_SETSIZE];
-    bzero(active_connections, FD_SETSIZE);
+	int active_fds[124];
+	bzero(active_fds, sizeof(active_fds));
 
-    while (1)
-    {
-        // Necessary because select is destructive
-        read_fds = read_fds_cpy;
+	while (1)
+	{
+		read_fds = read_fds_cpy;
 
-        if (select(FD_SETSIZE, &read_fds, NULL, NULL, NULL) < 0)
-        {
-            ft_print_str(2, "Fatal error\n");
-            exit(1);
-        }
+		if (select(124, &read_fds, 0, 0, 0) < 0)
+		{
+			ft_putstr(2, "Fatal error\n");
+			close(sockfd);
+			exit(1);
+		}
 
-        for (int i = 3; i <= FD_SETSIZE; ++i)
-        {
-            if (FD_ISSET(i, &read_fds))
-            {
-                // New connection waiting to be accepted
-                if (i == sockfd)
-                {
-                    struct sockaddr_in cli;
-                    len = sizeof(cli);
-                    connfd = accept(sockfd, (struct sockaddr *)&cli, (socklen_t *)&len);
-                    if (connfd < 0)
-                    {
-                        ft_print_str(2, "Fatal error\n");
-                        exit(1);
-                    }
-                    else
-                    {
-                        add_to_array(active_connections, connfd);
-                        FD_SET(connfd, &read_fds_cpy);
-                        print_to_clients("server: client %d just arrived\n", active_connections, connfd);
-                    }
-                }
-                // Existing connection sending data
-                else
-                {
-                    char *buf = (char *)malloc(sizeof(char) * 30000);
-                    bzero(buf, 30000);
+		for (int i = 3; i < 124; ++i)
+		{
+			if (FD_ISSET(i, &read_fds))
+			{
+				if (i == sockfd)
+				{
+					struct sockaddr_in cli;
+					bzero(&cli, sizeof(cli));
+					int len = sizeof(cli);
+					int connfd = accept(sockfd, (struct sockaddr *)&cli, (socklen_t *)&len);
+					if (connfd < 0)
+					{
+						ft_putstr(2, "Fatal error\n");
+						close(sockfd);
+						exit(1);
+					}
 
-                    int bytes_recv = recv(i, buf, 30000, 0);
-                    switch (bytes_recv)
-                    {
-                    case -1:
-                        ft_print_str(2, "Fatal error\n");
-                        if (buf)
-                            free(buf);
-                        exit(1);
-                    case 0:
-                    {
-                        print_to_clients("server: client %d just left\n", active_connections, i);
-                        remove_from_array(active_connections, i);
-                        FD_CLR(i, &read_fds_cpy);
-                        close(i);
+					printf("server acccept the client %d...\n", connfd);
+					FD_SET(connfd, &read_fds_cpy);
+					ft_add_connection(active_fds, connfd);
 
-                        // if (buf)
-                        //     free(buf);
-                        break;
-                    }
-                    }
+					int j = 0;
+					while (active_fds[j])
+					{
+						if (active_fds[j] != connfd && active_fds[j] != -1)
+							ft_putint(active_fds[j], "server: client %d just arrived\n", ft_find_connection(active_fds, connfd));
+						j++;
+					}
+				}
+				else
+				{
+					char *buf = (char *)malloc(sizeof(char) * 30000);
+					bzero(buf, 30000);
 
-                    int must_break = 0;
-                    while (!must_break)
-                    {
-                        char *msg = (char *)malloc(sizeof(char) * 30000);
-                        bzero(msg, 30000);
+					int bytes_rec = recv(i, buf, 30000, 0);
+					if (bytes_rec < 0)
+					{
+						ft_putstr(2, "Error with recv\n");
+						exit(1);
+					}
+					else if (bytes_rec == 0)
+					{
+						int j = 0;
+						while (active_fds[j])
+						{
+							if (active_fds[j] != i && active_fds[j] != -1)
+								ft_putint(active_fds[j], "server: client %d just left\n", ft_find_connection(active_fds, i));
+							j++;
+						}
 
-                        int extract_res = extract_message(&buf, &msg);
-                        if (extract_res == -1)
-                        {
-                            ft_print_str(2, "Fatal error\n");
-                            if (msg)
-                                free(msg);
-                            return 1;
-                        }
-                        else if (extract_res == 0)
-                        {
-                            must_break = 1;
-                            if (strlen(buf) == 0)
-                                break;
+						FD_CLR(i, &read_fds_cpy);
+						ft_remove_connection(active_fds, i);
+						close(i);
 
-                            msg = buf;
-                        }
+						continue;
+					}
 
-                        char string[] = "client %d: ";
-                        char *output = (char *)malloc(sizeof(char) * (strlen(string) + strlen(msg) + 50));
-                        strcat(output, string);
-                        strcat(output, msg);
-                        print_to_clients(output, active_connections, i);
-                        free(output);
+					while (buf && strlen(buf) != 0)
+					{
+						char *msg = (char *)malloc(sizeof(char) * 30000);
+						bzero(msg, 30000);
+						if (ft_find_newline(buf) == -1)
+						{
+							strcpy(msg, buf);
+							free(buf);
+						}
+						else
+						{
+							int ret = extract_message(&buf, &msg);
+							if (ret == -1)
+							{
+								ft_putstr(2, "Error with extract_message\n");
+								exit(1);
+							}
+							else if (ret == 0)
+							{
+								printf("msg is .%s.\n", msg);
+								printf("buf is .%s.\n", buf);
+								printf("Is zero:\n");
+							}
+						}
 
-                        if (must_break)
-                            break;
-
-                        free(msg);
-                    }
-                    if (buf)
-                        free(buf);
-                }
-            }
-        }
-    }
+						int j = 0;
+						while (active_fds[j])
+						{
+							if (active_fds[j] != i && active_fds[j] != -1)
+							{
+								ft_putint(active_fds[j], "Client %d: ", i);
+								if (send(active_fds[j], msg, strlen(msg), 0) < 0)
+								{
+									ft_putstr(2, "Error with send\n");
+									exit(1);
+								}
+							}
+							j++;
+						}
+						free(msg);
+					}
+				}
+			}
+		}
+	}
 }
