6ceee98ffd424d13dcd8de6a9fd7a71bd2d61906
Nicolas Gasco <73175085+nicolasgasco@users.noreply.github.com>
2024-01-29 22:06:59 +0000

    Improve comments in ft_strlen

--- 22_libasm/ft_strlen.s
+++ 22_libasm/ft_strlen.s
@@ -4,10 +4,10 @@ global  ft_strlen
 ft_strlen:
     mov rbx, rax ; get the first argument
     mov rax, 0d  ; initialize the counter
-    jmp .loop
+    jmp .loop    ; start the loop
 
 .loop:
-    cmp byte [rbx], 0d ; check if the current byte is 0
+    cmp byte [rbx], 0d ; check if the current byte is \0
     je  .end           ; if it is, jump to the end
     inc rbx            ; increment the pointer
     inc rax            ; increment the counter
