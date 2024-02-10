21a321296f031a83a32a05536311303ddac97e27
Nicolas Gasco <73175085+nicolasgasco@users.noreply.github.com>
2024-02-10 21:18:13 +0000

    Add ft_strcmp with tests

--- 22_libasm/ft_libasm.h
+++ 22_libasm/ft_libasm.h
@@ -3,5 +3,6 @@
 
 size_t ft_strlen(const char *s);
 char *ft_strcpy(char *restrict dst, const char *restrict src);
+int ft_strcmp(const char *s1, const char *s2);
 
 #endif
--- 22_libasm/main.c
+++ 22_libasm/main.c
@@ -73,11 +73,50 @@ void ft_strcpy_tests()
     }
 }
 
+void ft_strcmp_assertion(char *s1, char *s2)
+{
+    printf("  - When s1 is '%s%.50s%s' and s2 is '%s%.50s%s'\n", BLUE, s1, NC, BLUE, s2, NC);
+
+    printf("    strcmp: %s%d%s, ft_strcmp: %s%d%s\n", YELLOW, strcmp(s1, s2), NC, YELLOW, ft_strcmp(s1, s2), NC);
+    printf("\n");
+}
+
+void ft_strcmp_tests()
+{
+    const char *cmp_seeds1[] = {
+        "Hello world!",
+        "a",
+        "String containing 👋🌍 emojis",
+        "Not a very long string, but still long enough to be considered long",
+        NULL};
+
+    const char *cmp_seeds2[] = {
+        "hello world!",
+        "",
+        "10",
+        " \t",
+        NULL};
+
+    ft_print_test_title("FT_STRCMP");
+
+    for (int i = 0; STRING_SEEDS[i]; i++)
+    {
+        ft_strcmp_assertion(STRING_SEEDS[i], cmp_seeds1[i]);
+    }
+
+    for (int i = 0; STRING_SEEDS[i]; i++)
+    {
+        ft_strcmp_assertion(STRING_SEEDS[i], cmp_seeds2[i]);
+    }
+}
+
 int main()
 {
     printf("\n%sLIBASM TESTS%s\n\n", YELLOW, NC);
 
     ft_strlen_tests();
     ft_strcpy_tests();
+    ft_strcmp_tests();
+
     return 0;
 }
