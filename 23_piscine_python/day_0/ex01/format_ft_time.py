873b93df3bf3e19a3ce46430c0c12e5ef374f248
Nicolas Gasco <nicolas.gasco92@gmail.com>
2024-02-12 20:37:00 +0100

    Add day_0/ex01

---
+++ 23_piscine_python/day_0/ex01/format_ft_time.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+import time as time
+
+now = datetime.now()
+
+full_date = now.strftime("%B %-d, %Y")
+total_seconds = time.time()
+short_date = now.strftime("%b %-d %Y")
+
+print(f"Seconds since {full_date}: {total_seconds:,} or {total_seconds:.2e} in scientific notation\n{short_date}")
