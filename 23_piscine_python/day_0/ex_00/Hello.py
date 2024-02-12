8c17b900508ce82f731e1af64fc71a46cf91d48d
Nicolas Gasco <nicolas.gasco92@gmail.com>
2024-02-12 20:36:06 +0100

    Add comments to day_0/ex00

--- 23_piscine_python/day_0/ex_00/Hello.py
+++ 23_piscine_python/day_0/ex_00/Hello.py
@@ -1,11 +1,14 @@
 ft_list = ["Hello", "tata!"]
 ft_list[1] = "World!"
 
+# Tuples are immutable, so we need to convert it to a list, then change the value, and then convert it back to a tuple
 ft_tuple = ("Hello", "toto!")
 ft_tuple = list(ft_tuple)
 ft_tuple[1] = "Spain!"
 ft_tuple = tuple(ft_tuple)
 
+# Sets are mutable, but we can't change the value of an element, so we need to remove the element and add a new one
+# Order is not guaranteed
 ft_set = {"Hello", "tutu!"}
 ft_set.remove("tutu!")
 ft_set.add("Bilbao!")
