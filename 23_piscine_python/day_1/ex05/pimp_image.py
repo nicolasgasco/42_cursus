0a558df7d7e71a036809d127f05e018f79aababb
Nicolas Gasco <nicolas.gasco92@gmail.com>
2024-02-18 16:22:51 +0100

    Complete day_1/ex05

--- 23_piscine_python/day_1/ex05/pimp_image.py
+++ 23_piscine_python/day_1/ex05/pimp_image.py
@@ -3,6 +3,16 @@ import matplotlib.pyplot as plt
 
 
 def _describe_image(image: numpy.ndarray) -> None:
+    """
+    Prints the shape and content of the given image.
+
+    Parameters:
+    image (numpy.ndarray): The image to describe.
+
+    Returns:
+    None
+    """
+
     print(f"The shape of image is: {image.shape}")
     print(image)
 
@@ -37,12 +47,13 @@ def ft_invert(array) -> None:
 
     for y in range(len(array)):
         for x in range(len(array[y])):
-            rgb_values = array[y][x]
+            rgb_values = array[y, x]
+
             red_value = rgb_values[0]
             green_value = rgb_values[1]
             blue_value = rgb_values[2]
 
-            new_image[y][x] = [MAX_RGB - red_value,
+            new_image[y, x] = [MAX_RGB - red_value,
                                MAX_RGB - green_value, MAX_RGB - blue_value]
 
     _save_image(new_image, 'inverted.jpg')
@@ -53,14 +64,15 @@ def ft_red(array) -> None:
     """
     Make the image red.
     """
+
     new_image = numpy.copy(array)
 
     for y in range(len(array)):
         for x in range(len(array[y])):
-            rgb_values = array[y][x]
+            rgb_values = array[y, x]
             red_value = rgb_values[0]
 
-            new_image[y][x] = [red_value, 0, 0]
+            new_image[y, x] = [red_value, 0, 0]
 
     _save_image(new_image, 'red.jpg')
     _describe_image(new_image)
@@ -70,14 +82,15 @@ def ft_green(array) -> None:
     """
     Make the image green.
     """
+
     new_image = numpy.copy(array)
 
     for y in range(len(array)):
         for x in range(len(array[y])):
-            rgb_values = array[y][x]
+            rgb_values = array[y, x]
             green_value = rgb_values[1]
 
-            new_image[y][x] = [0, green_value, 0]
+            new_image[y, x] = [0, green_value, 0]
 
     _save_image(new_image, 'green.jpg')
     _describe_image(new_image)
@@ -87,14 +100,15 @@ def ft_blue(array) -> None:
     """
     Make the image blue.
     """
+
     new_image = numpy.copy(array)
 
     for y in range(len(array)):
         for x in range(len(array[y])):
-            rgb_values = array[y][x]
+            rgb_values = array[y, x]
             blue_value = rgb_values[2]
 
-            new_image[y][x] = [0, 0, blue_value]
+            new_image[y, x] = [0, 0, blue_value]
 
     _save_image(new_image, 'blue.jpg')
     _describe_image(new_image)
@@ -109,13 +123,13 @@ def ft_grey(array) -> None:
 
     for y in range(len(array)):
         for x in range(len(array[y])):
-            rgb_values = array[y][x]
+            rgb_values = array[y, x]
             red_value = rgb_values[0]
             green_value = rgb_values[1]
             blue_value = rgb_values[2]
 
             new_value = red_value / 3.0 + green_value / 3.0 + blue_value / 3.0
-            new_image[y][x] = [new_value, new_value, new_value]
+            new_image[y, x] = [new_value, new_value, new_value]
 
     _save_image(new_image, 'greyscale.jpg', True)
     _describe_image(new_image)
