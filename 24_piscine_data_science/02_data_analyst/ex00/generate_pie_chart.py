81013a7a3cdc30f7243d9abd01f8d35f72326a6c
Nicolas Gasco <nicolas.gasco92@gmail.com>
2024-02-27 22:53:00 +0100

    Complete ex00 of 02_data_analyst

---
+++ 24_piscine_data_science/02_data_analyst/ex00/generate_pie_chart.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+df = pd.read_csv("../customers.csv")
+
+event_counts = df['event_type'].value_counts()
+print(event_counts)
+
+plt.pie(event_counts, labels=event_counts.index.tolist(), autopct='%1.1f%%')
+plt.show()
+plt.savefig('event_types.jpg', bbox_inches='tight', pad_inches=0, dpi=300)
