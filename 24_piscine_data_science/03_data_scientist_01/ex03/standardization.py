f6bccc78ace06750d7065d63a1df87d7b99c0923
Nicolas Gasco <nicolas.gasco92@gmail.com>
2024-03-25 20:12:36 +0100

    Complete ex03 of 24_piscine_data_science

--- 24_piscine_data_science/03_data_scientist_01/ex03/standardization.py
+++ 24_piscine_data_science/03_data_scientist_01/ex03/standardization.py
@@ -3,34 +3,54 @@ import matplotlib.pyplot as plt
 
 
 def main():
-    jedi_data = pd.read_csv("../Train_knight.csv")
+    files = ["Train_knight.csv", "Test_knight.csv"]
 
-    jedi_data_without_column = jedi_data.drop(columns=["knight"])
-    mean = jedi_data_without_column.mean()
-    std = jedi_data_without_column.std()
-    standardized_data = (jedi_data_without_column - mean) / std
+    for file in files:
 
-    standardized_data = standardized_data.join(jedi_data["knight"])
+        raw_data = pd.read_csv(f"../{file}")
+        data = raw_data
 
-    plt.clf()
-    jedi_knight_data = standardized_data[standardized_data["knight"] == "Jedi"]
-    sith_knight_data = standardized_data[standardized_data["knight"] == "Sith"]
-    plt.scatter(jedi_knight_data["Empowered"],
-                jedi_knight_data["Stims"], alpha=0.4,
-                color='blue', label="Jedi")
-    plt.scatter(sith_knight_data["Empowered"],
-                sith_knight_data["Stims"], alpha=0.4,
-                color='red', label="Sith")
-    plt.xlabel("Empowered")
-    plt.ylabel("Stims")
-    plt.legend(loc='upper left')
+        has_knight = False
+        if "knight" in data.columns:
+            has_knight = True
+            data = data.drop(columns=["knight"])
 
-    plt.ylim(-1.99, 4.8)
-    plt.xlim(-1.99, 2.99)
+        mean = data.mean()
+        std = data.std()
+        standardized_data = (data - mean) / std
 
-    plt.show()
-    plt.savefig('knight_stims_empowered.jpg',
-                bbox_inches='tight', pad_inches=0, dpi=500)
+        plt.clf()
+        if has_knight:
+            standardized_data = standardized_data.join(raw_data["knight"])
+
+            jedi_knight_data = standardized_data[standardized_data["knight"] == "Jedi"]
+            sith_knight_data = standardized_data[standardized_data["knight"] == "Sith"]
+            plt.scatter(jedi_knight_data["Empowered"],
+                        jedi_knight_data["Stims"], alpha=0.4,
+                        color='blue', label="Jedi")
+            plt.scatter(sith_knight_data["Empowered"],
+                        sith_knight_data["Stims"], alpha=0.4,
+                        color='red', label="Sith")
+            plt.xlabel("Empowered")
+            plt.ylabel("Stims")
+            plt.legend(loc='upper left')
+
+            plt.ylim(-1.99, 4.8)
+            plt.xlim(-1.99, 2.99)
+
+            plt.show()
+            plt.savefig("knight_stims_empowered-standardized.jpg",
+                        bbox_inches='tight', pad_inches=0, dpi=500)
+        else:
+            plt.scatter(standardized_data["Empowered"],
+                        standardized_data["Stims"], alpha=0.4,
+                        color='green')
+
+            plt.show()
+            plt.savefig("jedi_stims_empowered-standardized.jpg",
+                        bbox_inches='tight', pad_inches=0, dpi=500)
+            
+        print(standardized_data)
 
 
 if __name__ == "__main__":
