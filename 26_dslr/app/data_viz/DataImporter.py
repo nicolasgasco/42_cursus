bf6b384bc3f80cbb667e829da37fce48020bb2a0
Nicolas Gasco <nicolas.gasco92@gmail.com>
2024-04-21 08:55:53 +0200

    Use absolute path for data importing

--- 26_dslr/app/data_viz/DataImporter.py
+++ 26_dslr/app/data_viz/DataImporter.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-DATA_PATH = "../../data/"
+DATA_PATH = "/dslr/data/"
 
 TRAIN_SET_FILENAME = "dataset_train.csv"
 TEST_SET_FILENAME = "dataset_test.csv"
@@ -11,8 +11,8 @@ class DataImporter:
         try:
             self.train_set = pd.read_csv(DATA_PATH + TRAIN_SET_FILENAME)
             self.test_set = pd.read_csv(DATA_PATH + TEST_SET_FILENAME)
-        except FileNotFoundError:
-            print("DataImporter: Error: file not found.")
+        except FileNotFoundError as e:
+            print(f"DataImporter: {e}")
             exit(1)
 
     def get_sets(self) -> tuple[pd.DataFrame, pd.DataFrame]:
