147de9b4c2c3fa6d9f7c259daf0735c8571bb5eb
Nicolas Gasco <nicolas.gasco92@gmail.com>
2024-04-23 22:52:07 +0200

    Add program to train model

--- 26_dslr/app/prediction/logreg_train.py
+++ 26_dslr/app/prediction/logreg_train.py
@@ -1,4 +1,7 @@
+import pandas as pd
 import sys as sys
+
+from LogisticRegression import LogisticRegression
 
 
 def main():
@@ -6,6 +9,13 @@ def main():
 
     if argc != 2:
         raise ValueError("Usage: python3 logreg_train.py <dataset>")
+
+    dataset_file_path = sys.argv[1]
+    train_set = pd.read_csv(dataset_file_path)
+
+    regression = LogisticRegression(train_set)
+
+    regression.train()
 
 
 if __name__ == "__main__":
