74d3e13d9f965842a2a8154368ff84f1161ee302
Nicolas Gasco <nicolas.gasco92@gmail.com>
2024-06-23 16:03:21 +0200

    Improve logic of DataPrepper class

--- 27_multilayer_perceptron/app/split.py
+++ 27_multilayer_perceptron/app/split.py
@@ -1,8 +1,5 @@
-import json as json
 import pandas as pd
-import sys as sys
 
-from colorama import Fore, Style
 from src.DataImporter import DataImporter
 from src.DataPrepper import DataPrepper
 
@@ -13,12 +10,12 @@ def main():
     data: pd.DataFrame = data_importer.import_data()
 
     data_prepper = DataPrepper(data)
-
-    validation_percentage: int | None = data_prepper.get_validation_percentage()
-    print(f"{Fore.YELLOW}{validation_percentage}%{Style.RESET_ALL} of the data will be used for validation.\n")
+    print(data_prepper)
 
     data_prepper.split()
     data_prepper.save_to_csv()
+
+    print(data_prepper)
 
 
 if __name__ == "__main__":
--- 27_multilayer_perceptron/app/src/DataPrepper.py
+++ 27_multilayer_perceptron/app/src/DataPrepper.py
@@ -1,6 +1,9 @@
 import json as json
+import re
 import pandas as pd
 import os as os
+
+from colorama import Fore, Style
 
 
 class InvalidPercentageError(Exception):
@@ -10,12 +13,29 @@ class InvalidPercentageError(Exception):
 class DataPrepper:
     def __init__(self, data: pd.DataFrame):
         def _parse_percentage() -> int:
+            """
+            Parses the validation percentage from the settings file.
+
+            Returns:
+                int: The validation percentage value.
+
+            Raises:
+                FileNotFoundError: If the settings file is missing.
+                InvalidPercentageError: If the percentage value is invalid.
+                ValueError: If the percentage value is not an integer.
+                KeyError: If the 'validation_percentage' key is missing in the settings file.
+            """
+
             try:
                 settings_dir_path: str | None = os.environ.get(
                     "SETTINGS_DIR_PATH")
                 assert settings_dir_path is not None, "DataPrepper: SETTINGS_DIR_PATH environment variable not set."
 
-                with open(settings_dir_path + "/split.json") as file:
+                settings_file_name = settings_dir_path + "/split.json"
+                print(
+                    f"Importing settings from {Fore.YELLOW}{settings_dir_path}/split.json{Style.RESET_ALL}...\n")
+
+                with open(settings_file_name) as file:
                     data = json.load(file)
             except FileNotFoundError:
                 raise FileNotFoundError("DataPrepper: Settings file missing.")
@@ -25,12 +45,14 @@ class DataPrepper:
                 percentage = int(validation_percentage_str)
                 if percentage < 0 or percentage > 90:
                     raise InvalidPercentageError(
+
                         "DataPrepper: Invalid percentage value.")
                 return percentage
             except ValueError:
                 raise ValueError("DataPrepper: Invalid percentage value.")
             except KeyError:
                 raise KeyError(
+
                     "DataPrepper: validation_percentage key missing.")
 
         assert data is not None, "DataPrepper: data is None."
@@ -46,10 +68,28 @@ class DataPrepper:
         self._validation_set: pd.DataFrame | None = None
         self._train_set: pd.DataFrame | None = None
 
+    def __str__(self) -> str:
+        representation = "DataPrepper("
+        representation += f"validation_percentage={self._validation_percentage}, "
+        representation += f"data={self._data.shape[0]}, "
+        representation += f"train_set={self._train_set.shape[0] if self._train_set is not None else None}, "
+        representation += f"validation_set={self._validation_set.shape[0] if self._validation_set is not None else None})"
+        representation += "\n"
+
+        return representation
+
     def split(self) -> None:
+        """
+        Splits the data into training and validation sets base
+        on the specified validation percentage.
+
+        Raises:
+            AssertionError: If the validation percentage is None.
+
+        """
         assert self._validation_percentage is not None, "DataPrepper: validation_percentage is None."
 
-        print("Splitting data into training and validation sets.")
+        print("Splitting data into training and validation sets...\n")
 
         total_rows = self._data.shape[0]
         upper_bound = total_rows * self._validation_percentage // 100 - 1
@@ -58,14 +98,28 @@ class DataPrepper:
         self._train_set = self._data.iloc[upper_bound:]
 
     def save_to_csv(self) -> None:
+        """
+        Saves the train and validation sets to CSV files.
+
+        Raises:
+            ValueError: If the train or validation set is not set.
+        """
+
         if self._train_set is None or self._validation_set is None:
             raise ValueError(
                 "DataPrepper: split method must be called before saving to CSV.")
 
-        self._train_set.to_csv(self._data_dir_path +
-                               "/train_set.csv", index=False)
-        self._validation_set.to_csv(
-            self._data_dir_path + "/validation_set.csv", index=False)
+        print("Saving train and validation sets to CSV files...")
+
+        train_set_path = self._data_dir_path + "/train_set.csv"
+        self._train_set.to_csv(train_set_path, index=False)
+        print(
+            f"Train set saved to {Fore.YELLOW}{train_set_path}{Style.RESET_ALL}.")
+
+        validation_set_path = self._data_dir_path + "/validation_set.csv"
+        self._validation_set.to_csv(validation_set_path, index=False)
+        print(
+            f"Validation set saved to {Fore.YELLOW}{validation_set_path}{Style.RESET_ALL}.\n")
 
     def get_train_set(self) -> pd.DataFrame | None:
         return self._train_set
