c2094f42121630e981d6e0db1daa8ac925502447
Nicolas Gasco <nicolas.gasco92@gmail.com>
2024-07-23 19:00:25 +0200

    Improve and document util functions

--- 27_multilayer_perceptron/app/src/utils.py
+++ 27_multilayer_perceptron/app/src/utils.py
@@ -2,13 +2,32 @@ import os as os
 import numpy as np
 
 
-def print_output(args):
+def print_output(*args, **kwargs) -> None:
+    """
+    Prints the given arguments
+    if the environment variable 'PRINT_OUTPUT' is set to '1'.
+
+    Args:
+      *args: Variable number of arguments to be printed.
+
+    Returns:
+      None
+    """
+
     should_print = os.getenv("PRINT_OUTPUT") == "1"
 
-    if (should_print):
-        print(args)
+    if should_print:
+        print(*args, **kwargs)
 
 
 def rand_small_float() -> float:
-    num: float = np.random.randn() * 0.1
+    """
+    Generate a random small float number.
+
+    Returns:
+      float: A random small float number.
+    """
+
+    num: float = np.random.randn() * 0.01
+
     return num
