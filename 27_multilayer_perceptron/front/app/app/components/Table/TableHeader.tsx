6f238fee3a33b3af2dd7f4d366acdb08986f7215
Nicolas Gasco <nicolas.gasco92@gmail.com>
2024-08-13 23:16:52 +0200

    Fix issue with TableHeader prop

--- 27_multilayer_perceptron/front/app/app/components/Table/TableHeader.tsx
+++ 27_multilayer_perceptron/front/app/app/components/Table/TableHeader.tsx
@@ -1,17 +1,15 @@
 interface TableHeaderProps {
   children: React.ReactNode;
-  index?: number;
   style?: React.CSSProperties;
 }
 
 export const TableHeader = ({
   children,
-  index,
   style,
   ...restProps
 }: TableHeaderProps): JSX.Element => {
   return (
-    <th key={index} className="px-6 pb-2 border-b-2" {...restProps}>
+    <th className="px-6 pb-2 border-b-2" {...restProps}>
       {children}
     </th>
   );
