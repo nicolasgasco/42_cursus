831bb0e69920268864035345d9ea5684f8fa9a96
Nicolas Gasco <nicolas.gasco92@gmail.com>
2025-05-04 22:56:16 +0200

    Fix Linter issues

--- 29_learn2slither/constants/__init__.py
+++ 29_learn2slither/constants/__init__.py
@@ -1,3 +1,11 @@
-from .board import *
-from .path import *
+from .board import BoardBlock
+from .game import SnakeDirection
+from .path import CONSTANTS_DIR_PATH, MAPS_DIR_PATH, SETTINGS_DIR_PATH
 
+__all__ = [
+    "BoardBlock",
+    "SnakeDirection",
+    "CONSTANTS_DIR_PATH",
+    "MAPS_DIR_PATH",
+    "SETTINGS_DIR_PATH",
+]
